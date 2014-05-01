9d22f74601720a87aa9336e84268b83b18b000fb
Chris Lloyd <christopher.lloyd@gmail.com>
2014-05-01 13:32:02 -0700

    Kill status updates from activity page.

--- app/controllers/products/dashboard_controller.rb
+++ app/controllers/products/dashboard_controller.rb
@@ -2,16 +2,11 @@ class Products::DashboardController < ApplicationController
   before_action :set_product
 
   def index
-    @status_update = status_update && status_update.decorate
     @stream_events = @product.stream_events.page(page)
     respond_to do |format|
       format.js   { render :layout => false }
       format.html
     end
-  end
-  
-  def status_update
-    @product.status_updates.first
   end
 
   protected
