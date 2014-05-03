f84560bb8d65f753eb73d8da6937cb57cbcebd5b
Dave Newman <dave@snappyco.de>
2014-05-02 18:09:33 -0700

    Fix minterworker bug

--- app/assets/javascripts/collections/users_collection.js.coffee
+++
@@ -1,5 +0,0 @@
-#= require backbone
-#= require models/user
-
-class window.UsersCollection extends Backbone.Collection
-  model: window.User
--- app/workers/minter_worker.rb
+++ app/workers/minter_worker.rb
@@ -5,6 +5,6 @@ class MinterWorker
     entry = TransactionLogEntry.find(transaction_log_entry_id)
     logger.info("action=#{entry.action} entry=#{entry.id} value=#{entry.value}")
 
-    Stake::EntryMinter.new(entry.product).mint_coins!(entry)
+    Stake::EntryMinter.new(entry.product, entry).mint_coins!
   end
 end
