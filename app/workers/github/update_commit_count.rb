4339b973abfc0f7fd4d7957258f79abcac9009fe
Dave Newman <dave@snappyco.de>
2014-05-08 17:12:32 -0700

    Fix update commit count worker for repo not found

--- app/workers/github/update_commit_count.rb
+++ app/workers/github/update_commit_count.rb
@@ -15,7 +15,13 @@ module Github
     end
 
     def stats(repo)
-      stats = Github::Worker.new.get("/repos/#{repo.full_name}/contributors") || []
+      stats = Github::Worker.new.get("/repos/#{repo.full_name}/contributors")
+      # will return {"message": "Not Found"} hash if not found
+      if stats.is_a? Hash
+        []
+      else
+        stats
+      end
     end
 
     def contributions_count(author)
