9f81b13a196388b63c2af89e6563c4724cc41761
Chris Lloyd <christopher.lloyd@gmail.com>
2014-05-14 10:54:47 -0700

    Format coin helper.

--- app/helpers/coin_helper.rb
+++ app/helpers/coin_helper.rb
@@ -1,8 +1,12 @@
-module CoinsHelper
+module CoinHelper
 
   def cents_to_coins(cents)
     coins = cents / 100.to_d
     number_with_precision(coins, precision: (coins.round == coins ? 0 : 2), delimiter: ',')
+  end
+
+  def format_coins(product, cents)
+    tag(:span, "#{cents_to_coins(cents)} #{product.name} coins", class: 'coins')
   end
 
 end
