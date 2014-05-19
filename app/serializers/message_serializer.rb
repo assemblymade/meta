200bb40c34115e0f585aa01e0c5ed2eabf2d20d1
Chris Lloyd <christopher.lloyd@gmail.com>
2014-05-19 09:47:16 -0700

    Cleanup a bunch of unused content.

--- app/serializers/message_serializer.rb
+++
@@ -1,9 +0,0 @@
-class MessageSerializer < BaseSerializer
-  include MarkdownHelper
-  attributes :body, :body_html
-  has_one :author
-
-  def body_html
-    unescaped_markdown(object.body)
-  end
-end
