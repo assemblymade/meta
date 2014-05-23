5dc3a2da8c4368a40874f428a183c647fc6092f2
Dave Newman <dave@snappyco.de>
2014-05-22 18:49:30 -0700

    really good emails is public

--- app/models/product.rb
+++ app/models/product.rb
@@ -93,7 +93,7 @@ class Product < ActiveRecord::Base
 
   serialize :repos, Repo::Github
 
-  PRIVATE = %w(asm coderwall asm-ideas really-good-emails)
+  PRIVATE = %w(asm coderwall asm-ideas)
   NON_PROFIT = %w(meta)
 
   class << self
