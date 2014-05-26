48223f4203ecb0958fbcff165920b03f89ad1f43
Chris Lloyd <christopher.lloyd@gmail.com>
2014-05-26 16:10:22 -0700

    Scrap the form builder.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,9 +1,5 @@
 module FormHelper
 
-  def form_for(record, options={}, &block)
-    super(record, {builder: ApplicationFormBuilder}, &block)
-  end
-
   def ajax_form_for(record, options = {}, &block)
     options = options.deep_merge(
       remote: true,
