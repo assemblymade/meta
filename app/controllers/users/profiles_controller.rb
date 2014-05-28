31c2073c849166c47c4d859bbc6d94ccbe441139
Dave Newman <dave@snappyco.de>
2014-05-27 18:09:27 -0700

    Added payment page back in

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -11,9 +11,24 @@ class Users::ProfilesController < ApplicationController
     respond_with current_user, location: user_path(current_user)
   end
 
+  def payment
+    @user = current_user.decorate
+
+    payment_option = current_user.payment_option || PaymentOption::PAYPAL
+    @payment_options = [{
+        name: 'Paypal (International)',
+        value: PaymentOption::PAYPAL,
+        selected: (payment_option == PaymentOption::PAYPAL)
+      }, {
+        name: 'Direct Deposit (U.S. Only)',
+        value: PaymentOption::ACH,
+        selected: (payment_option == PaymentOption::ACH)
+      }]
+  end
+
   # private
 
   def user_params
-    params.require(:user).permit(:payment_option, :paypal_email, :bank_account_id, :bank_name, :bank_last4, :address_line1, :address_line2, :address_city, :address_state, :address_zip)
+    params.require(:user).permit(:payment_option, :paypal_email, :bank_account_id, :bank_name, :bank_last4, :address_line1, :address_line2, :address_city, :address_state, :address_zip, :address_country)
   end
 end
