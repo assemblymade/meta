657bdcbc1ab17f951853115a2f1cc5e2cf992aed
Dave Newman <dave@snappyco.de>
2014-05-30 12:41:49 -0700

    integrating mailgun

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 require './lib/rack/cross_origin_assets'
+require './lib/mailgun/rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
