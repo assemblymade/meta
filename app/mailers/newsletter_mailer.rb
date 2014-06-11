22f834263f98067ddb92c90b8415028005720bfd
Austin Smith <awwstn@gmail.com>
2014-06-11 14:58:10 -0700

    changed newsletter to come from awwstn

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -6,7 +6,7 @@ class NewsletterMailer < BaseMailer
     @newsletter = Newsletter.find(newsletter_id)
     @user = User.find(user_id)
     mail to: @user.email_address,
-         from: 'chrislloyd <chris@assemblymade.com>',
+         from: 'awwwstn <austin@assemblymade.com>',
          subject: @newsletter.subject
   end
 
