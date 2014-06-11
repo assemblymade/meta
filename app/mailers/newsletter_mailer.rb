39d88c55f34eac3b725ff355d2a843e597c7a5ff
Austin Smith <awwstn@gmail.com>
2014-06-11 16:25:06 -0700

    Change newsletter "from" field

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -6,7 +6,7 @@ class NewsletterMailer < BaseMailer
     @newsletter = Newsletter.find(newsletter_id)
     @user = User.find(user_id)
     mail to: @user.email_address,
-         from: 'Austin Smith (@awwstn) <austin@assemblymade.com>',
+         from: 'Austin from Assembly <austin@assemblymade.com>',
          subject: @newsletter.subject
   end
 
