378cc79af1f8f5f38072af050e371c725d68e169
Dave Newman <dave@snappyco.de>
2014-06-20 15:12:49 -0700

    cache transaction log csv

--- app/controllers/stakes_controller.rb
+++ app/controllers/stakes_controller.rb
@@ -2,7 +2,7 @@ class StakesController < ApplicationController
   before_action :set_product
 
   def show
-    @report = TransactionLogReport.new(TransactionLogEntry.where(product_id: @product.id).order(:created_at).to_a)
+    @report = TransactionLogReport.new(@product)
     respond_to do |format|
       format.csv { render csv: @report, filename: @product.slug, layout: false }
     end
