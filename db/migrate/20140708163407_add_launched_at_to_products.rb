c3ab567f7fe3d59ba44163d6036b24862d30c263
Dave Newman <dave@snappyco.de>
2014-07-08 18:21:55 -0700

    Set a few select products to launched

--- db/migrate/20140708163407_add_launched_at_to_products.rb
+++ db/migrate/20140708163407_add_launched_at_to_products.rb
@@ -1,5 +1,26 @@
 class AddLaunchedAtToProducts < ActiveRecord::Migration
   def change
     add_column :products, :launched_at, :datetime
+
+    launched = %w(
+      asm
+      barrtr
+      be-stow
+      buckets
+      coderwall
+      confy
+      dawn
+      family-table
+      firesize
+      helpful
+      meta
+      present
+      readraptor
+      really-good-emails
+      sharebay
+      wivern
+    )
+
+    Product.where(slug: launched).update_all launched_at: Time.now
   end
 end
