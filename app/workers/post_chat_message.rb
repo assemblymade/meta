95ac2f69dc1b03ef86b30dde8f6d008b68f26cd8
Charles Pletcher <cpletcher2@gmail.com>
2014-07-18 16:50:32 -0700

    Post kernel messages to /hubot/asm

--- app/workers/post_chat_message.rb
+++ app/workers/post_chat_message.rb
@@ -3,7 +3,7 @@ class PostChatMessage < ApiWorker
 
   def perform(product_slug, message)
     @product = Product.find_by(slug: product_slug)
-
+    # TODO: Double-check this query, make it more efficient
     return false unless Activity.where(target_id: @product.id)
                                 .where.not(type: 'Activities::Chat')
                                 .where.not(type: 'Activities::FoundProduct')
@@ -11,7 +11,7 @@ class PostChatMessage < ApiWorker
 
     @user = User.find_by(username: 'kernel')
 
-    post "#{ENV['HUBOT_HOST']}/kernel",
+    post "#{ENV['HUBOT_HOST']}/hubot/asm",
       message: message,
       product: @product.slug
   end
