db75d8eefcbbeef330fbedfff4d661296ad2b779
Dave Newman <dave@snappyco.de>
2014-07-25 11:09:50 -0700

    Fix textcompletes spec

--- app/controllers/api/textcompletes_controller.rb
+++ app/controllers/api/textcompletes_controller.rb
@@ -3,7 +3,12 @@ module Api
     respond_to :json
 
     def index
-      @product = Product.find_by(id: params[:product_id])
+      @product = if params[:product_id].uuid?
+        Product.find(params[:product_id])
+      else
+        Product.find_by!(slug: params[:product_id])
+      end
+
       textcompletes = { textcompletes: TextcompleteSearch.call(@product, params[:query]) }
       respond_with textcompletes
     end
