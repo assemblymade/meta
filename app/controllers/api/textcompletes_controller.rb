4cccc9cb3c7b88b47bcb7fb2d956c0433cdac451
Dave Newman <dave@snappyco.de>
2014-07-24 19:39:57 -0700

    fix broken autocomplete

--- app/controllers/api/textcompletes_controller.rb
+++ app/controllers/api/textcompletes_controller.rb
@@ -1,10 +1,9 @@
 module Api
   class TextcompletesController < ProductController
-    before_action :find_product!
-
     respond_to :json
 
     def index
+      @product = Product.find_by(id: params[:product_id])
       textcompletes = { textcompletes: TextcompleteSearch.call(@product, params[:query]) }
       respond_with textcompletes
     end
