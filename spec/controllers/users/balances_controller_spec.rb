e29a406bfe74052181f0ae7cbd59f83b4b8aed65
Dave Newman <dave@snappyco.de>
2014-08-08 12:01:58 -0700

    Fix balance controller spec

--- spec/controllers/users/balances_controller_spec.rb
+++ spec/controllers/users/balances_controller_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe Users::BalancesController do
+  let(:user) { User.make! }
+
   describe '#show' do
     it 'is successful' do
+      sign_in user
       get :show
       expect(response).to be_successful
     end
-  end
-  
-  describe '#withdraw' do
-    
   end
 end
