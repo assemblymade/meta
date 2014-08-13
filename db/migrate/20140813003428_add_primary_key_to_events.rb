89f2e46355e14a9ceaabe73e8b4fd3e8fc408dd6
Dave Newman <dave@snappyco.de>
2014-08-12 17:36:12 -0700

    add primary key to events...

---
+++ db/migrate/20140813003428_add_primary_key_to_events.rb
@@ -0,0 +1,9 @@
+class AddPrimaryKeyToEvents < ActiveRecord::Migration
+def up
+    execute "ALTER TABLE events ADD PRIMARY KEY (id);"
+  end
+
+  def down
+    execute "ALTER TABLE events DROP CONSTRAINT table_pkey;"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809171241) do
+ActiveRecord::Schema.define(version: 20140813003428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
