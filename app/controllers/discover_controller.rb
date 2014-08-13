6035c079cc964967789b74597b55de54c474650d
Dave Newman <dave@snappyco.de>
2014-08-13 14:51:07 -0700

    remove flagged posts from recent posts

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -52,6 +52,8 @@ class DiscoverController < ApplicationController
   def blog
     @posts = Post.joins(:product).
       where('products.flagged_at is null').
-      order(created_at: :desc).page(params[:page])
+      order(created_at: :desc)
+
+    @page = @posts.page(params[:page])
   end
 end
