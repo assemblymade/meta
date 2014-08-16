e231e6c664ff9eb7f26fef2d30690f6484816c68
Charles Pletcher <cpletcher2@gmail.com>
2014-08-16 07:38:07 -0700

    Fix connection to Launchpad

--- app/workers/github/worker.rb
+++ app/workers/github/worker.rb
@@ -70,7 +70,7 @@ module Github
     end
 
     def launchpad_connection
-      Faraday.new(url: 'https://launchpad.assembly.com') do |faraday|
+      Faraday.new(url: 'http://launchpad.assembly.com') do |faraday|
         faraday.adapter  :net_http
       end
     end
