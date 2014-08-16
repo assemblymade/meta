a7e23dd5c6f022bfa93c451c6f1916fe0a988112
Dave Newman <dave@snappyco.de>
2014-08-16 10:34:18 -0700

    log gc stats after each sidekiq job

--- lib/sidekiq/middleware/log_stats.rb
+++ lib/sidekiq/middleware/log_stats.rb
@@ -17,7 +17,27 @@ class Sidekiq::Middleware::LogStats
   end
 
   def log_result(start, result)
-    logger.info { "count#sidekiq.success=1 sample#sidekiq.job.time=#{elapsed(start)}ms sample#sidekiq.enqueued=#{Sidekiq::Stats.new.enqueued}" }
+    logger.info {
+      gc_stats = GC.stat.slice(
+        :heap_used,
+        :heap_length,
+        :heap_increment,
+        :heap_live_num,
+        :heap_free_num,
+        :heap_final_num,
+        :total_allocated_object,
+        :total_freed_object,
+        :malloc_increase,
+        :malloc_limit,
+        :oldmalloc_increase,
+        :oldmalloc_limit
+      ).map{|k,v| "sample#gc_#{k}=#{v}" }
+
+      (gc_stats + [ "count#sidekiq.success=1",
+        "sample#sidekiq.job.time=#{elapsed(start)}ms",
+        "sample#sidekiq.enqueued=#{Sidekiq::Stats.new.enqueued}"
+      ]).join(' ')
+    }
 
   end
 
