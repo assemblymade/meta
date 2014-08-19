2cdc2f51d94b415ce5b5785c05fb5653ebdf1d3b
Dave Newman <dave@snappyco.de>
2014-08-19 16:24:24 -0700

    Add additional attributes to user serializer

--- app/serializers/user_analytics_serializer.rb
+++ app/serializers/user_analytics_serializer.rb
@@ -1,9 +1,30 @@
 class UserAnalyticsSerializer < ActiveModel::Serializer
-  attributes :created_at, :email, :location, :customer_id, :last_sign_in_at, :facebook_uid
+  attributes :created_at, :email, :location, :username, :last_sign_in_at, :last_request_at
+  attributes :facebook_uid, :username, :twitter_nickname, :github_login
 
-  attributes :comments
-  attributes :ideas
-  attributes :wips
+  attributes :payment_via
+  attributes :partner_of, :core_team_of
+
+  attributes :comments, :following, :ideas, :wips
+
+  def payment_via
+    object.payment_option.try(:type)
+  end
+
+  def core_team_of
+    object.team_memberships.core_team.joins(:product).pluck('products.name')
+  end
+
+  def partner_of
+    Product.
+       joins('inner join transaction_log_entries tle on tle.product_id = products.id').
+       group('products.name').
+       where('wallet_id = ?', object.id).pluck(:name)
+  end
+
+  def following
+    object.watchings.where(watchable_type: Product).size
+  end
 
   def comments
     Event::Comment.where(user: object).size
