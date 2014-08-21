4c3472735971a2415aea122f853713fc8d831c92
Matthew Deiters <mdeiters@gmail.com>
2014-08-21 12:50:45 -0700

    tweaking styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820004340) do
+ActiveRecord::Schema.define(version: 20140821193036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
