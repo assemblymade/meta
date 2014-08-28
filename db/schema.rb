e861a64367631a263e29fe246a63d6fe08f6b819
Dave Newman <dave@snappyco.de>
2014-08-28 11:01:15 -0700

    Move description from first comment to bounty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827184855) do
+ActiveRecord::Schema.define(version: 20140828175016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
