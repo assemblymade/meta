f3fd90b62328ad4f0fb6f0ce4ac98cb28c6c868f
Charles Pletcher <cpletcher2@gmail.com>
2014-09-03 21:38:11 -0700

    Make AllowWorkerIdToBeNull migration reversible

--- db/migrate/20140904012629_allow_worker_id_to_be_null.rb
+++ db/migrate/20140904012629_allow_worker_id_to_be_null.rb
@@ -1,5 +1,9 @@
 class AllowWorkerIdToBeNull < ActiveRecord::Migration
-  def change
+  def up
     change_column :transaction_log_entries, :work_id, :uuid, null: true
+  end
+
+  def down
+    change_column :transaction_log_entries, :work_id, :uuid, null: false, default: SecureRandom.uuid
   end
 end
