a891ce64480c3c9f50fcf567326c52cbd351256d
Dave Newman <dave@snappyco.de>
2014-09-06 19:53:47 -0700

    fix updates route

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ ASM::Application.routes.draw do
                                action: 'trending'
                              },
                              constraints: {
-                               action: /trending|live/
+                               action: /trending|live|updates/
                              }
 
   get '/discover/tech/:tech' => 'discover#tech'
