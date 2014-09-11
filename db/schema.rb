90545373d6d4d4cee07d05a997e3cf3f38c18a13
vanstee <patrick@vanstee.me>
2014-09-11 11:58:59 -0700

    Cut down activity story_id queries by 2 orders of magnitude

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911184707) do
+ActiveRecord::Schema.define(version: 20140911185710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140911184707) do
     t.uuid     "story_id"
   end
 
+  add_index "activities", ["story_id"], name: "index_activities_on_story_id", using: :btree
   add_index "activities", ["target_id"], name: "index_activities_on_target_id", using: :btree
 
   create_table "allocation_events", id: false, force: true do |t|
