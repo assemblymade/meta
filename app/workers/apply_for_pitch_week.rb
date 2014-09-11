7f67649b9241d6bcecd88f4e9a43a2f00b9a9c96
vanstee <patrick@vanstee.me>
2014-09-11 11:26:06 -0700

    Send out the awaiting approval email when submitted to pitch week

--- app/workers/apply_for_pitch_week.rb
+++ app/workers/apply_for_pitch_week.rb
@@ -2,9 +2,11 @@ class ApplyForPitchWeek
   include Sidekiq::Worker
 
   def perform(product_id, applicant_id)
-    PitchWeekApplication.create!(
+    application = PitchWeekApplication.create!(
       product_id: product_id,
       applicant_id: applicant_id
     )
+
+    PitchWeekMailer.awaiting_approval(application.id).deliver
   end
 end
