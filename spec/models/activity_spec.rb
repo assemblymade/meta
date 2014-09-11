f05a206d77491088f8e33a8e2f494f4910b430d0
Dave Newman <dave@snappyco.de>
2014-09-11 14:30:55 -0700

    Fix bug with activity not pushing to segment

---
+++ spec/models/activity_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Activity do
+  let(:actor) { User.make! }
+  let(:subject) { Event::Comment.make! }
+  let(:target) { Task.make! }
+
+  it 'tracks after create' do
+    expect {
+      activity = Activity.create!(actor: actor, subject: subject, target: target)
+      activity.run_callbacks(:commit)
+    }.to change(TrackActivityCreated.jobs, :size).by(1)
+
+  end
+end
