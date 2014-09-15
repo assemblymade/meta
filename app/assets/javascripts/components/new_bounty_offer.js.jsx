398f5d99cf55d5dceb03edba4812940befe36d77
Charles Pletcher <cpletcher2@gmail.com>
2014-09-15 13:39:57 -0700

    Update bounty offer slider It now shows value as a factor of the average bounty's value It's color-coded And it's fuzziness increases from hundredths to tenths to quarters of a percent as you move up the scale

--- app/assets/javascripts/components/new_bounty_offer.js.jsx
+++ app/assets/javascripts/components/new_bounty_offer.js.jsx
@@ -1,4 +1,5 @@
 (function() {
+  var classSet = React.addons.classSet;
   var min = 1
   var scale = 1.05
   var initialVal = 25
@@ -7,7 +8,27 @@
     mixins: [React.addons.LinkedStateMixin],
 
     componentDidMount: function() {
-      this.props.onChange(this.scaleOffer(initialVal))
+      this.props.onChange(this.scaleOffer(initialVal));
+    },
+
+    descriptionClasses: function(factor) {
+      return classSet({
+        'text-success': (factor <= 0.9),
+        'text-primary': (factor > 0.9 && factor < 1.1),
+        'text-warning': (factor > 1.1)
+      });
+    },
+
+    factor: function() {
+      var factor = this.props.newOffer / this.props.averageBounty;
+
+      if (factor <= 0.5) {
+        return factor;
+      } else if (factor <= 1) {
+        return Math.floor((factor * 10)) / 10;
+      }
+
+      return Math.floor((factor * 4)) / 4;
     },
 
     render: function() {
@@ -18,43 +39,47 @@
         </div>
         <input type="range" min="0" max="100" onChange={this.handleOfferChanged} defaultValue={initialVal} />
         <div className="row">
-          <span className="text-coins text-small col-md-2">
-            <span className="icon icon-app-coin"></span>
-            {numeral(this.props.newOffer).format('0,0')}
-          </span>
-          <span className="text-right text-small col-md-10">
-            <span className="text-success">
-              {this.relativeSize()}
+
+          <span className="text-right text-small col-md-12">
+            <span className="text-coins text-small" style={{"margin-right": "5px"}}>
+              <span className="icon icon-app-coin"></span>
+              {numeral(this.props.newOffer).format('0,0')}
             </span>
-            <span> the average {this.props.product.name} bounty.</span>
+            ({this.relativeSize()}
+            <span> the average {this.props.product.name} bounty</span>)
           </span>
         </div>
       </div>
     },
 
     handleOfferChanged: function(e) {
-      this.props.onChange(this.scaleOffer(e.target.value))
-    },
-
-    scaleOffer: function(val) {
-      var multiplier = (this.props.maxOffer - min) / (Math.pow(scale, 99) - 1)
-      var shift = min - multiplier
-      return Math.min(
-        Math.pow(scale, val) * multiplier + shift, this.props.maxOffer
-      )
+      this.props.onChange(this.scaleOffer(e.target.value));
     },
 
     relativeSize: function() {
-      var factor = this.props.newOffer / this.props.averageBounty
-      if (factor <= 0.9) {
-        return numeral(factor).format('0%') + ' of'
-      } else if (factor > 1.1) {
-        return numeral(factor - 1).format('0%') + ' more than'
-      } else {
-        return 'Similar size to'
-      }
+      var factor = this.factor();
+      var descriptor;
+
+      return (
+        <span>
+          <strong>
+            <span className={this.descriptionClasses(factor)}>
+              {numeral(factor).format('0.00a')}x
+            </span>
+          </strong>
+        </span>
+      );
+    },
+
+    scaleOffer: function(val) {
+      var multiplier = (this.props.maxOffer - min) / (Math.pow(scale, 99) - 1);
+      var shift = min - multiplier;
+
+      return Math.min(
+        Math.pow(scale, val) * multiplier + shift, this.props.maxOffer
+      );
     }
-  })
+  });
 
   if (typeof module !== 'undefined') {
     module.exports = NewBountyOffer;
