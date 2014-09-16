f4714ec1cc9f376e0fab556270dba9c8eb9b4e0c
Charles Pletcher <cpletcher2@gmail.com>
2014-09-16 16:39:40 -0700

    Be more descriptive about creating a repo vs. creating a repo with Launchpad

--- app/assets/javascripts/components/new_repository_preview.js.jsx
+++ app/assets/javascripts/components/new_repository_preview.js.jsx
@@ -5,11 +5,31 @@
   var xhr = require('../xhr');
 
   var NewRepositoryPreview = React.createClass({
+    buttonState: function() {
+      return this.state.inputPreview.length >= 2 ? false : true;
+    },
+
+    componentDidMount: function() {
+      $('#create-with-launchpad').tooltip({
+        container: 'body'
+      });
+    },
+
+    componentWillUnmount: function() {
+      $('#create-with-launchpad').tooltip('destroy');
+    },
+
     getInitialState: function() {
       return {
         inputPreview: '',
         transform: this.props.transform || this.transform
       };
+    },
+
+    handleHide: function() {
+      this.setState({
+        popoverShown: false
+      });
     },
 
     handleResponse: function(err, response) {
@@ -18,42 +38,6 @@
       }
 
       window.location = window.location;
-    },
-
-    render: function() {
-      return (
-        <form role="form" class="form">
-          <FormGroup>
-            <div className="input-group" style={{ width: '50%' }}>
-              <input type="text"
-                  className="form-control"
-                  value={this.state.inputPreview}
-                  placeholder={this.props.placeholder}
-                  onChange={this.onChange} />
-              <span className="input-group-btn">
-                <a
-                    href="javascript:"
-                    className="btn btn-primary"
-                    onClick={this.onClick(false)}
-                    disabled={this.buttonState()}>
-                  Create
-                </a>
-                <a
-                    href="javascript:"
-                    className="btn btn-primary"
-                    onClick={this.onClick(true)}
-                    disabled={this.buttonState()}
-                    data-toggle="tooltip">
-                  Launchpad
-                </a>
-              </span>
-            </div>
-            <p className="text-muted omega" style={{ 'margin-top': '5px', 'margin-left': '1px' }}>
-              Preview: <strong>{this.props.addonText + this.state.inputPreview}</strong>
-            </p>
-          </FormGroup>
-        </form>
-      );
     },
 
     onChange: function(e) {
@@ -77,9 +61,41 @@
       }.bind(this);
     },
 
-
-    buttonState: function() {
-      return this.state.inputPreview.length >= 2 ? false : true;
+    render: function() {
+      return (
+        <form role="form" className="form">
+          <FormGroup>
+            <div className="input-group" style={{ width: '70%' }}>
+              <input type="text"
+                  className="form-control"
+                  value={this.state.inputPreview}
+                  placeholder={this.props.placeholder}
+                  onChange={this.onChange} />
+              <span className="input-group-btn">
+                <a
+                    href="javascript:"
+                    className="btn btn-primary"
+                    onClick={this.onClick(false)}
+                    disabled={this.buttonState()}>
+                  Create empty repo
+                </a>
+                <a  id="create-with-launchpad"
+                    href="javascript:"
+                    className="btn btn-primary"
+                    onClick={this.onClick(true)}
+                    disabled={this.buttonState()}
+                    data-toggle="tooltip"
+                    title="Let Assembly generate a landing page automatically using GitHub Pages.">
+                  Create with landing page
+                </a>
+              </span>
+            </div>
+            <p className="text-muted omega" style={{ 'margin-top': '5px', 'margin-left': '1px' }}>
+              Preview: <strong>{this.props.addonText + this.state.inputPreview}</strong>
+            </p>
+          </FormGroup>
+        </form>
+      );
     },
 
     transform: function(text) {
