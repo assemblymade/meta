cbf7014b2bb31065e9ac1e7035140436cbbbb68a
Charles Pletcher <cpletcher2@gmail.com>
2014-09-17 18:51:42 -0700

    Make sure that users who already have accounts can claim their Assembly Assets
    This route is terrible, and the whole solution is kind of hacky, but
    it's the quickest way I could think to work around Rails defaults.

--- app/controllers/assembly_assets_controller.rb
+++ app/controllers/assembly_assets_controller.rb
@@ -3,7 +3,26 @@ class AssemblyAssetsController < ProductController
   before_action :set_product
 
   def create
-    unless AssemblyAsset.find_by(user: current_user, product: @product).where('promo_redeemed_at is not null').any?
+    unless AssemblyAsset.find_by(user: current_user, product: @product).try(:where, 'promo_redeemed_at is not null').try(:any?)
+      if @product == Product.find_by_slug('assemblycoins')
+        asset = AssemblyAsset.new(
+          product: @product,
+          user: current_user,
+          amount: 10,
+          promo_redeemed_at: Time.now
+        )
+
+        asset.grant!(promo=true)
+
+        flash[:first_assets_granted] = true
+      end
+    end
+
+    redirect_to product_path(@product)
+  end
+
+  def email_promo
+    unless AssemblyAsset.find_by(user: current_user, product: @product).try(:where, 'promo_redeemed_at is not null').try(:any?)
       if @product == Product.find_by_slug('assemblycoins')
         asset = AssemblyAsset.new(
           product: @product,
