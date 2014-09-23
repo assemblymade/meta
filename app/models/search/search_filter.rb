41b29ab90af79232509ec14d1be0b631db99920b
Dave Newman <dave@snappyco.de>
2014-09-23 14:50:59 -0700

    debugging search

--- app/models/search/search_filter.rb
+++ app/models/search/search_filter.rb
@@ -1,7 +1,7 @@
 module Search
   class SearchFilter < Struct.new(:name, :slug)
     def self.find(slug)
-      all.find{|t| t.slug == slug } || raise(ActiveRecord::RecordNotFound, slug)
+      all.find{|t| t.slug == slug }
     end
 
     def self.find_by_name(name)
