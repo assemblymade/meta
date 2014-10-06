cf8660fc403f5062017ad6a6f1291f7cd4b4d559
Chris Lloyd <christopher.lloyd@gmail.com>
2014-10-06 12:19:25 -0700

    Redirect new users to welcome survey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_up_path_for_user
-    session[:previous_url] || discover_path
+    session[:previous_url] || url_for(controller: 'surveys', action: 'new')
   end
 
   def after_sign_out_path_for_user
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for_user
-    after_sign_up_path_for_user
+    session[:previous_url] || discover_path
   end
 
 
