b8b6382028382ecce46b6a4ebb80aece81e53e28
Charles Pletcher <cpletcher2@gmail.com>
2014-10-07 15:43:21 -0700

    Move default descriptions to one line
    The Markdown parser thinks they're code otherwise

--- app/workers/create_project.rb
+++ app/workers/create_project.rb
@@ -17,24 +17,15 @@ class CreateProject < ApiWorker
         milestone_tasks_attributes: [
           {
             title: "Write the problem and value proposition",
-            description: "Having a clear product vision is important will greatly help users in the community understand the product. 
-            This bounty is for writing down the goals and description of the project.\n
-            - Visit #{Rails.application.routes.url_helpers.edit_product_path(@product)}\n 
-            - Write 1-3 sentences about the problem that the product aims to solve, and how it will solve the problem.
-            Save this in the project description.\n
-            - Fill out the Vision section for the product."
+            description: "Having a clear product vision is important will greatly help users in the community understand the product. This bounty is for writing down the goals and description of the project.\n - Visit #{Rails.application.routes.url_helpers.edit_product_path(@product)}\n - Write 1-3 sentences about the problem that the product aims to solve, and how it will solve the problem. Save this in the project description.\n - Fill out the Vision section for the product."
           },
           {
             title: "Choose the tech stack",
-            description: "- Choose the language and frameworks to build the project with. \n
-                - This should include the APIs, libraries, languages, and services that are needed.\n
-                    - See this bounty for an example of a completed bounty for scoping out the tech stack (https://assembly.com/pay-it-forward/bounties/1)"
+            description: "- Choose the language and frameworks to build the project with. \n - This should include the APIs, libraries, languages, and services that are needed.\n - See this bounty for an example of a completed bounty for scoping out the [tech stack](https://assembly.com/pay-it-forward/bounties/1)"
           },
           {
             title: "Commit a README file to the repository",
-            description: "This bounty will help new contributors get up to speed with the product and the tech stack. \n
-            - Post a description of the tech stack and the required APIs, libraries, and other dependencies in a README.md file.\n
-            - Commit this README.md file to the repository and push it up."
+            description: "This bounty will help new contributors get up to speed with the product and the tech stack. \n - Post a description of the tech stack and the required APIs, libraries, and other dependencies in a README.md file.\n - Commit this README.md file to the repository and push it up."
           }
         ]
       }
