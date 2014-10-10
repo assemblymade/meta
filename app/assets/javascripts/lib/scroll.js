999ca8f4855ca2b1dadada7dd212a6c38c17607a
vanstee <patrick@vanstee.me>
2014-10-10 15:17:07 -0400

    Add the submit for review button as planned

---
+++ app/assets/javascripts/lib/scroll.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('[data-scroll]').click(function() {
+    var $target = $($(this).attr('data-target'));
+
+    $('html, body').animate({ scrollTop: $target.offset().top }, 500);
+
+    setTimeout(function() { $target.focus(); }, 500);
+
+
+    return false;
+  });
+});
