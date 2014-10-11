31cebbd0e658f1b442c768ed05b998816c374700
barisser <barisser@gmail.com>
2014-10-10 17:56:48 -0700

    added transfer functions to worker

--- app/workers/cryptoworker.rb
+++ app/workers/cryptoworker.rb
@@ -45,7 +45,7 @@ class CryptoWorker
 
       body={}
       body['from_public_address'] = product.wallet_public_address
-      body['to_public_address'] = user_id.wallet_public_address
+      body['to_public_address'] = user.wallet_public_address
       body['fee_each'] = ENV.fetch("STANDARD_BTC_FEE")
       body['from_private_key'] = product.wallet_private_key
       body['issuing_address'] = product.wallet_public_address
@@ -55,6 +55,19 @@ class CryptoWorker
   end
 
   def transfer_coins(product_id, giver_user_id, recipient_user_id, coins)
+    product = Product.find_by(id: product_id)
+    sender = User.find_by(id: giver_user_id)
+    receiver = User.find_by(id: recipient_user_id)
+
+    body={}
+    body['from_public_address'] = sender.wallet_public_address
+    body['to_public_address'] = receiver.wallet_public_address
+    body['fee_each'] = ENV.fetch("STANDARD_BTC_FEE")
+    body['from_private_key'] = sender.wallet_private_key
+    body['issuing_address'] = product.wallet_public_address
+    body['transfer_amount'] = coins.to_s
+
+    response = post("https://coins.assembly.com/v1/transactions/transfer", body.to_json)
 
   end
 
