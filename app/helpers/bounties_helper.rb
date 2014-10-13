43bc398dd6c1b7a0536a5b0cd190dff38784b5a2
vanstee <patrick@vanstee.me>
2014-10-13 11:19:31 -0400

    Add tags filter to bounties index

--- app/helpers/bounties_helper.rb
+++ app/helpers/bounties_helper.rb
@@ -5,12 +5,14 @@ module BountiesHelper
     user = options[:user] || params[:user]
     sort = options[:sort] || params[:sort]
     project = options[:project] || params[:project]
+    tag = options[:tag] || params[:tag]
 
     product_wips_path(product,
       state:   state,
       user:    user,
       sort:    sort,
-      project: project
+      project: project,
+      tag:     tag
     )
   end
 
