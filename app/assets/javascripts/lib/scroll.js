f0164e4fa24684e53f091cf4c1391099d4fc9e00
vanstee <patrick@vanstee.me>
2014-10-13 19:39:07 -0400

    Fix the scroll button now that we inject that after page load

--- app/assets/javascripts/lib/scroll.js
+++ app/assets/javascripts/lib/scroll.js
@@ -1,11 +1,9 @@
 $(function() {
-  $('[data-scroll]').click(function() {
+  $('body').delegate('[data-scroll]', 'click', function() {
     var $target = $($(this).attr('data-target'));
 
     $('html, body').animate({ scrollTop: $target.offset().top }, 500);
-
     setTimeout(function() { $target.focus(); }, 500);
-
 
     return false;
   });
