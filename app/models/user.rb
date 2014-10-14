aa20021d758260d6fd381e1a8ca6f1daf67b5341
barisser <barisser@gmail.com>
2014-10-14 13:32:29 -0700

    changed syntax in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,7 @@ class User < ActiveRecord::Base
   before_save :ensure_authentication_token
 
   after_commit -> { Indexer.perform_async(:index, User.to_s, self.id) }, on: :create
-  on_create :assign_key_pair!
+  after_commit :assign_key_pair!, on: :create
 
   # default users to immediate email
   MAIL_DAILY = 'daily'
