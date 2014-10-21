b88d448f545f38a8bc6b7cce18816ad694fe4607
Charles Pletcher <cpletcher2@gmail.com>
2014-10-21 14:01:41 -0700

    Use default responder

--- app/controllers/news_feed_items_controller.rb
+++ app/controllers/news_feed_items_controller.rb
@@ -2,16 +2,7 @@ class NewsFeedItemsController < ProductController
   before_action :find_product!
 
   def index
-    @news_feed_items = ActiveModel::ArraySerializer.new(
-      @product.news_feed_items.order(updated_at: :desc),
-      each_serializer: NewsFeedItemSerializer
-    ).as_json
-  end
-
-  def all
-    @news_feed_items = ActiveModel::ArraySerializer.new(
-      NewsFeedItem.take(20).order(updated_at: :desc),
-      each_serializer: NewsFeedItemSerializer
-    ).as_json
+    @news_feed_items = @product.news_feed_items.order(updated_at: :desc)
+    respond_with @news_feed_items
   end
 end
