19f65a8c03d4ad8843ad73321b2c48cf0b725578
barisser <barisser@gmail.com>
2014-10-22 11:42:51 -0700

    refactored with dave small stuff, color url returns nil if not found

--- lib/open_assets/cryptodata.rb
+++ lib/open_assets/cryptodata.rb
@@ -2,18 +2,9 @@ module OpenAssets
   class Cryptodata
 
     def color_url(product_id)
-      wallet_public_address = Product.find_by(id: product_id).wallet_public_address
-
-        url = "https://coins.assembly.com/colors/null"
-
-      if not wallet_public_address.nil?
-
-        url = "https://coins.assembly.com/colors/"+ wallet_public_address
-
+      if wallet_public_address = Product.find_by(id: product_id).wallet_public_address
+        "https://coins.assembly.com/colors/#{wallet_public_address}"
       end
-
-      return url
-
     end
 
   end
