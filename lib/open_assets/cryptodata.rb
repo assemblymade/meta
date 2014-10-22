b51cfb4fda1b8adb8194fc515dce39a6ce03a4b2
barisser <barisser@gmail.com>
2014-10-22 11:45:00 -0700

    pass product object not id to color url

--- lib/open_assets/cryptodata.rb
+++ lib/open_assets/cryptodata.rb
@@ -1,8 +1,8 @@
 module OpenAssets
   class Cryptodata
 
-    def color_url(product_id)
-      if wallet_public_address = Product.find_by(id: product_id).wallet_public_address
+    def color_url(product)
+      if wallet_public_address = product.wallet_public_address
         "https://coins.assembly.com/colors/#{wallet_public_address}"
       end
     end
