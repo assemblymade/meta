7191a72e91d2a31249b16667c30c77693c7119a0
Charles Pletcher <cpletcher2@gmail.com>
2014-10-24 11:35:42 -0700

    Prevent Kernel from showing up in the News Feed

--- app/models/news_feed_item.rb
+++ app/models/news_feed_item.rb
@@ -6,10 +6,14 @@ class NewsFeedItem < ActiveRecord::Base
   has_many :news_feed_item_comments
 
   def self.create_with_target(target)
-    create!(
-      product: target.product,
-      source_id: target.user.id,
-      target: target
-    )
+    # Prevent @kernel from appearing in the News Feed
+    # (gross)
+    unless target.user.username == 'kernel'
+      create!(
+        product: target.product,
+        source_id: target.user.id,
+        target: target
+      )
+    end
   end
 end
