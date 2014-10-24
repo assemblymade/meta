e0e200367d6b0e3540b13769027b9a9463cd202a
vanstee <patrick@vanstee.me>
2014-10-24 16:27:31 -0400

    Partition news feed items into columns

--- app/assets/javascripts/components/news_feed/news_feed.js.jsx
+++ app/assets/javascripts/components/news_feed/news_feed.js.jsx
@@ -7,8 +7,18 @@
     render: function() {
       return (
         <div className="container mt2 mb4">
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 col-xs-12">
+          <div className="row hidden-xs">
+            <div className="col-sm-6">
+              {this.renderLeftNewsFeedItems()}
+            </div>
+
+            <div className="col-sm-6">
+              {this.renderRightNewsFeedItems()}
+            </div>
+          </div>
+
+          <div className="row visible-xs">
+            <div className="col-xs-12">
               {this.renderNewsFeedItems()}
             </div>
           </div>
@@ -16,11 +26,25 @@
       )
     },
 
-    renderNewsFeedItems: function() {
-      if(!this.props.news_feed_items) {
-        return this.renderEmpty()
-      }
+    renderLeftNewsFeedItems: function() {
+      return this.partitionedNewsFeedItems()[0].map(function(item) {
+        return NewsFeedItem(item);
+      })
+    },
 
+    renderRightNewsFeedItems: function() {
+      return this.partitionedNewsFeedItems()[1].map(function(item) {
+        return NewsFeedItem(item);
+      })
+    },
+
+    partitionedNewsFeedItems: function() {
+      return _.partition(this.props.news_feed_items, function(item) {
+        return this.props.news_feed_items.indexOf(item) % 2 == 0
+      }.bind(this))
+    },
+
+    renderNewsFeedItems: function() {
       return this.props.news_feed_items.map(function(item) {
         return NewsFeedItem(item);
       })
