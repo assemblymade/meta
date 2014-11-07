76a77340c3c85350abf547c005308f2e70056991
barisser <barisser@gmail.com>
2014-11-06 18:05:36 -0800

    up to date

--- spec/models/chronicle_spec.rb
+++ spec/models/chronicle_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-# describe Chronicle do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Chronicle do
+  Chronicle.new
+end
--- spec/models/deed_spec.rb
+++ spec/models/deed_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-# describe Deed do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Deed do
+  Deed.new
+end
