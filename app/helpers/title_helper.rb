1d4e21e90dbb000bb86f9332234cfe8e05b40098
Chris Lloyd <christopher.lloyd@gmail.com>
2014-11-19 09:42:01 -0800

    Add better description to bounties

--- app/helpers/title_helper.rb
+++ app/helpers/title_helper.rb
@@ -2,10 +2,19 @@ module TitleHelper
 
   SITE_TITLE = 'Assembly'
   TITLE_SEPARATOR = ' · '
+  DESCRIPTION_CHARACTER_LIMIT = 140
 
   def title(*parts)
     parts << SITE_TITLE unless signed_in?
     provide(:title, parts.compact.join(TITLE_SEPARATOR))
+  end
+
+  def description(text)
+    short_desc = truncate(text,
+      length: DESCRIPTION_CHARACTER_LIMIT,
+      separator: ' '
+    )
+    provide(:description, short_desc)
   end
 
 end
