2393829fde5fe7fe4806db0a8b128e24e38b4949
barisser <barisser@gmail.com>
2014-11-21 10:34:46 -0800

    be able to merge similar marks and migrate markings

--- app/models/make_marks.rb
+++ app/models/make_marks.rb
@@ -16,4 +16,16 @@ class MakeMarks
     end
     mark_it(object, themark)
   end
+
+  def merge_marks(destroyed_mark_name, into_existing_mark_name)
+    if destroyed_mark = Mark.find_by(name: destroyed_mark_name) && into_existing_mark = Mark.find_by(name: into_existing_mark_name)
+      old_markings = Marking.where(mark_id: destroyed_mark.id)
+      old_markings.each do |o|
+        o.update!({mark_id: into_existing_mark.id})
+      end
+      destroyed_mark.destroy
+    end
+  end
+
+
 end
