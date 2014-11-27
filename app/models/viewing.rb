24fb932834d900058cbb73ee6fcb3498a781b2a0
barisser <barisser@gmail.com>
2014-11-26 17:19:38 -0800

    removed delete all viewing and float weight validation

--- app/models/viewing.rb
+++ app/models/viewing.rb
@@ -1,5 +1,4 @@
 class Viewing < ActiveRecord::Base
   belongs_to :user
   belongs_to :viewable, polymorphic: true
-  validates :weight, presence: true
 end
--- db/migrate/20141126235932_add_float_weight_to_viewings.rb
+++ db/migrate/20141126235932_add_float_weight_to_viewings.rb
@@ -1,6 +1,5 @@
 class AddFloatWeightToViewings < ActiveRecord::Migration
   def change
-    Viewing.all.delete_all 
     add_column :viewings, :weight, :float
 
     add_column :top_bounties, :wip_id, :uuid
