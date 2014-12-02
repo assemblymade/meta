f3705a99d6c99b6fef6c45335a19431405c5917f
Ben <hello@benshyong.com>
2014-12-01 18:21:21 -0800

    idea page markdown

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -22,6 +22,21 @@ module MarkdownHelper
     @default_pipeline.call(text)[:output].to_s.html_safe
   end
 
+  def idea_markdown(text)
+    @comment_pipeline ||= HTML::Pipeline.new(PRODUCT_FILTERS,
+      asset_root: 'https://a248.e.akamai.net/assets.github.com/images/icons',
+      whitelist: html_whitelist,
+      firesize_url: ENV['FIRESIZE_URL'],
+      users_base_url: File.join(EXTENDER.root_url, 'users'))
+    begin
+      result = @comment_pipeline.call(text)
+      result[:output].to_s.html_safe
+    rescue => e
+      Rails.logger.error("pipeline=#{e.message} text=#{text}")
+      text
+    end
+  end
+
   # this is used in mailers, so use full urls
   def product_markdown(product, text)
     @product_pipeline ||= HTML::Pipeline.new(PRODUCT_FILTERS,
--- app/serializers/idea_comment_serializer.rb
+++ app/serializers/idea_comment_serializer.rb
@@ -9,6 +9,6 @@ class IdeaCommentSerializer < ApplicationSerializer
   end
 
   def markdown_body
-    markdown(body)
+    idea_markdown(body)
   end
 end
