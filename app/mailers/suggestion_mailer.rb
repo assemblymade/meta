dc30c09ede3170282e0457224d65066719648ba5
barisser <barisser@gmail.com>
2014-12-03 10:21:04 -0800

    removed development code

--- app/mailers/suggestion_mailer.rb
+++ app/mailers/suggestion_mailer.rb
@@ -2,7 +2,7 @@ class SuggestionMailer < ActionMailer::Base
   default from: "barisser@assembly.com"
 
   def create(user_id)
-    mailgun_campaign 'bounty_suggestions'
+    #mailgun_campaign 'bounty_suggestions'
 
     if @user = User.find(user_id)
       @username = @user.username
@@ -10,9 +10,9 @@ class SuggestionMailer < ActionMailer::Base
       @bounties = @user.top_bountys.pluck(:wip_id).map{|a| Wip.find(a) }.take(3)
       @tags = @user.user_identity.get_mark_vector.take(5)
 
-      mailgun_tag "bounty_suggestions"
+      #mailgun_tag "bounty_suggestions"
 
-      prevent_delivery(@user)
+      #prevent_delivery(@user)
 
       if @products.count >0 and @bounties.count >0 and @tags.count >0
         mail to: @user.email_address,
