70e6a522d947a5f392c5460f85dd28f9f8bace94
Charles Pletcher <cpletcher2@gmail.com>
2014-12-03 19:04:15 -0800

    Set updated_at and created_at in ApplicationSerializer

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,7 +1,7 @@
 class ApplicationSerializer < ActiveModel::Serializer
 
   attributes :type, :id
-  attributes :created, :updated
+  attributes :created, :created_at, :updated, :updated_at
 
   def type
     object.class.name.underscore
@@ -12,11 +12,14 @@ class ApplicationSerializer < ActiveModel::Serializer
   end
 
   def created_at
-    object.created_at.try(:iso8601)
+    object.created_at
   end
 
   def updated
     object.updated_at.try(:iso8601) if object.respond_to?(:updated_at)
   end
 
+  def updated_at
+    object.updated_at if object.respond_to?(:updated_at)
+  end
 end
