e204b4a525ba5fa81a3e99662ea57c156f08eac1
barisser <barisser@gmail.com>
2014-12-04 18:09:26 -0800

    changed state open requirement to closed at nil for suggested wips

---
+++ app/models/user_cluster.rb
@@ -0,0 +1,14 @@
+class UserCluster < ActiveRecord::Base
+  has_many :users
+
+  def self.calculate_center
+    my_mark_vector = []
+    self.users.each do |user|
+      user_vector = user.user_identity.get_mark_vector
+      scalar = 1.0 / self.users.count.to_f
+      scaled_user_vector = QueryMarks.new.scale_mark_vector(user_vector, scalar)
+      QueryMarks.new.add_mark_vectors(my_mark_vector, scaled_user_vector)
+    end
+  end
+
+end
