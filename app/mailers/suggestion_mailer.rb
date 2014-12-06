a826f9165e85a11f25532fd7d620a5466947eb9d
Ben <hello@benshyong.com>
2014-12-05 17:34:07 -0800

    first pass on suggestion mailer

--- app/mailers/suggestion_mailer.rb
+++ app/mailers/suggestion_mailer.rb
@@ -1,5 +1,7 @@
 class SuggestionMailer < BaseMailer
-  layout 'email'
+  layout 'suggested_bounties'
+  helper FiresizeHelper
+  helper AppIconHelper
 
   def create(user_id)
     mailgun_campaign 'suggestions'
@@ -9,6 +11,7 @@ class SuggestionMailer < BaseMailer
     @products = @user.top_products.pluck(:product_id).map{|a| Product.find(a) }.take(3)
     @bounties = @user.top_bountys.where('rank < 4').map{|a| Wip.find(a.wip_id)}
     @tags = @user.user_identity.get_mark_vector.take(5)
+    @body_style = "background:#f4f4f5;"
 
     mailgun_tag "suggestions"
 
