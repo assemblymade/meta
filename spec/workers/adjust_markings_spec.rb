965a46df1eeaa902552eda8036c74d79444c4be1
barisser <barisser@gmail.com>
2014-12-08 15:53:19 -0800

    fixed test on adjustmarkings

--- spec/workers/adjust_markings_spec.rb
+++ spec/workers/adjust_markings_spec.rb
@@ -6,11 +6,11 @@ describe AdjustMarkings do
 
   describe '#perform' do
     it 'adjusts markings for test user' do
-      Mark.create!({name: 'DENMARK'})
-      Marking.create!({mark: mark, markable: product})
+      mark = Mark.create!({name: 'DENMARK'})
+      Marking.create!({mark: mark, markable: product, weight: 2.0})
 
       expect {
-        AdjustMarkings.new.perform(user.id, product.id, "Product", 0.0)
+        AdjustMarkings.new.perform(user.id, product.id, "Product", 1.0)
       }.to change(Marking, :count).by(1)
     end
   end
