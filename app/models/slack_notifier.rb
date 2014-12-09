d7f964e146659efd33073fa064bdc5877a69fd93
Ben <hello@benshyong.com>
2014-12-09 09:50:50 -0800

    fix error in link

--- app/models/slack_notifier.rb
+++ app/models/slack_notifier.rb
@@ -39,7 +39,7 @@ class SlackNotifier
   def self.first_chat_message(user, chat_room_slug)
     url = Rails.application.routes.url_helpers.chat_room_url(
             ChatRoom.find_by(slug: 'general'), 
-            message: "Hey @#{user}!"
+            message: "Hey @#{user.username}!"
           )
     cta_link = "<#{url}|Greet me in #{chat_room_slug.titleize} chat.>"
 
