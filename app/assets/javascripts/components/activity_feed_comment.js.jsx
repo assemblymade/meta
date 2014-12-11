5d4154484928d994d3595b2cf06fb4425ac9fd32
Charles Pletcher <cpletcher2@gmail.com>
2014-12-11 15:05:22 -0800

    Use old styles for last comments on activity feed NFIs

---
+++ app/assets/javascripts/components/activity_feed_comment.js.jsx
@@ -0,0 +1,33 @@
+var Markdown = require('./markdown.js.jsx');
+
+var ActivityFeedComment = React.createClass({
+  displayName: 'ActivityFeedComment',
+
+  propTypes: {
+    author: React.PropTypes.object.isRequired,
+    body: React.PropTypes.string.isRequired,
+    heartable: React.PropTypes.bool
+  },
+
+  render: function() {
+    var author = this.props.author;
+    var body = this.props.body;
+
+    return (
+      <div className="clearfix py2">
+         <div className="left mr2 mt1">
+           <Avatar user={author} size={18} />
+         </div>
+         <div className="overflow-hidden gray-2">
+           <a className="bold black" href={author.url}>{author.username}</a>
+
+           <div className="activity-body">
+             <Markdown content={body} normalized={true} />
+           </div>
+         </div>
+       </div>
+    );
+  }
+});
+
+module.exports = ActivityFeedComment;
