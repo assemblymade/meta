6432da629ffc5e8874386813aa0292852c93c225
barisser <barisser@gmail.com>
2014-12-12 15:05:40 -0800

    rake migrated, getting travis failures from schema ?

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141212213316) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
