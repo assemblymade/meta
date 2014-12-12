0e8c058b125211dbe4888a91a8bea0ff467d9e50
barisser <barisser@gmail.com>
2014-12-12 15:00:42 -0800

    fixing merge conflcits

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 
 ActiveRecord::Schema.define(version: 20141212213316) do
 
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
