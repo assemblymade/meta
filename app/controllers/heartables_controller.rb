be7cab9a3557925e9acf0a5818462e23a8bd0fe3
Dave Newman <dave@snappyco.de>
2014-12-15 09:59:27 -0800

    don't assume unhearted is available

--- app/controllers/heartables_controller.rb
+++ app/controllers/heartables_controller.rb
@@ -32,7 +32,7 @@ class HeartablesController < ApplicationController
   def unlove
     @heart = Heart.find_by(heartable_id: heart_params[:id], user: current_user)
     if @heart
-      @heart.heartable.unhearted if @heart.destroy
+      @heart.heartable.try(:unhearted) if @heart.destroy
       render json: {
         heartable_id: @heart.heartable_id,
         heartable_type: @heart.heartable_type,
