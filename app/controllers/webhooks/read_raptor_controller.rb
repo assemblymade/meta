59be403f9ef819ee8c9a9e91cab947fee9eb720e
Dave Newman <dave@snappyco.de>
2014-12-18 11:13:43 -0800

    Email thread subscribers when new comments are created

--- app/controllers/webhooks/read_raptor_controller.rb
+++ app/controllers/webhooks/read_raptor_controller.rb
@@ -3,24 +3,20 @@ class Webhooks::ReadRaptorController < WebhookController
   def immediate
     user = User.find(params["user"])
 
-    entities.each do |entity|
-      # by default, readraptor will send us every unread item a user has.
-      # we're going to ignore all of them except the one we're interested in
-      if entity.id == params['entity_id']
-        entity.notify_by_email(user)
-        Rails.logger.info "readraptor_notify key=#{params["pending"]} user=#{user.username} entity=#{entity.id}"
-      end
+    # by default, readraptor will send us every unread item a user has.
+    # we're going to ignore all of them except the one we're interested in
+    if entity = find_entity(params['entity_id'])
+      entity.notify_by_email(user)
+      Rails.logger.info "readraptor_notify key=#{params["pending"]} user=#{user.username} entity=#{entity.id}"
     end
 
     render nothing: true, status: 200
   end
 
-  def entities
-    (params["pending"] || []).map{|id| id.split('_') }.map do |type, id, tag|
-      # we should ignore any tags, the main content article has no tag
-      if tag.nil?
-        type.constantize.find(id) rescue nil
-      end
-    end.compact
+  def find_entity(entity_id)
+    # we should ignore any tags, the main content article has no tag
+    if args = (params["pending"] || []).map{|id| id.split('_') }.find{|_, id, tag| id == entity_id && tag.nil? }
+      args[0].constantize.find(args[1]) rescue nil
+    end
   end
 end
--- spec/controllers/webhooks/read_raptor_controller_spec.rb
+++ spec/controllers/webhooks/read_raptor_controller_spec.rb
@@ -2,25 +2,44 @@ require 'spec_helper'
 
 describe Webhooks::ReadRaptorController do
   let(:user) { User.make! }
-  let(:product) { Product.make! }
-  let(:wips) { [product.tasks.make!, product.tasks.make!] }
-  let(:comments) { [wips[0].comments.make!, wips[1].comments.make!] }
 
   describe 'immediate' do
-    before do
-      post :immediate,
-        user: user.id,
-        entity_id: wips.first.id,
-        pending: (wips.map{|o| "#{o.class}_#{o.id}" } + comments.map{|o| "#{o.class}_#{o.id}" })
+    context 'wip' do
+      let(:product) { Product.make! }
+      let(:wips) { [product.tasks.make!, product.tasks.make!] }
+      let(:comments) { [wips[0].comments.make!, wips[1].comments.make!] }
+
+      before do
+        post :immediate,
+          user: user.id,
+          entity_id: wips.first.id,
+          pending: (wips.map{|o| "#{o.class}_#{o.id}" } + comments.map{|o| "#{o.class}_#{o.id}" })
+      end
+
+      it "returns 200" do
+        expect(response.response_code).to eq(200)
+      end
+
+      it 'notifies by email' do
+        expect_mail_queued(WipMailer, :wip_created, user.id, wips.first.id)
+      end
     end
 
-    it "returns 200" do
-      expect(response.response_code).to eq(200)
-    end
+    context 'nfi' do
+      let(:target) { Post.make! }
+      let(:nfi) { target.news_feed_item }
+      let(:comment) { nfi.comments.create!(user: User.make!, body: 'hay hay') }
 
-    it 'notifies by email' do
-      expect_mail_queued(WipMailer, :wip_created, user.id, wips.first.id)
+      before do
+        post :immediate,
+          user: user.id,
+          entity_id: comment.id,
+          pending: ["NewsFeedItemComment_#{comment.id}"]
+      end
+
+      it 'notifies by email' do
+        expect_mail_queued(CommentMailer, :new_comment, user.id, comment.id)
+      end
     end
   end
-
 end
