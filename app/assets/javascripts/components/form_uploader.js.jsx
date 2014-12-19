93570e19ce35b4e1a03396f25927d3ecdc3502aa
Charles Pletcher <cpletcher2@gmail.com>
2014-12-19 10:26:42 -0800

    Fix attachment uploading -- no more Access Denied errors

--- app/assets/javascripts/components/form_uploader.js.jsx
+++ app/assets/javascripts/components/form_uploader.js.jsx
@@ -1,78 +1,74 @@
-/** @jsx React.DOM */
+var AttachmentActionCreators = require('../actions/attachment_action_creators');
+var AttachmentStore = require('../stores/attachment_store');
 
-(function() {
-  var FormUploader = React.createClass({
-    getDefaultProps: function() {
-      return {
-        inputName: 'attachment',
-        url: $('meta[name=attachment-upload-url]').attr('content')
-      }
-    },
+var FormUploader = React.createClass({
+  componentDidMount: function() {
+    this.dropzone = new Dropzone(this.getDOMNode(), {
+      url: this.props.url,
+      accept: this.handleAccept(this.props.inputId),
+      sending: this.handleSending,
+      clickable: this.refs.select.getDOMNode()
+    });
 
-    getInitialState: function() {
-      return {
-        attachments: []
-      }
-    },
+    AttachmentStore.addChangeListener(this.getAttachment);
+  },
 
-    componentDidMount: function() {
-      this.dropzone = new Dropzone(this.getDOMNode(), {
-        url: this.props.url,
-        accept: this.handleAccept,
-        sending: this.handleSending,
-        clickable: this.refs.select.getDOMNode()
-      });
-    },
+  getAttachment: function() {
+    var attachment = AttachmentStore.getAttachment(this.props.inputId);
 
-    render: function() {
-      return (
-        <div className="dropzone-standalone">
-          <div className="dropzone-inner" ref="select">
-            {this.attachmentInputs()}
-            To attach files drag &amp; drop here or <a href="javascript:;">select files
-            from your computer</a>...
-          </div>
+    this.updateState({
+      attachments: { $push: [attachment.id] }
+    });
+  },
+
+  getDefaultProps: function() {
+    return {
+      inputName: 'attachment',
+      url: $('meta[name=attachment-upload-url]').attr('content')
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      attachments: []
+    }
+  },
+
+  render: function() {
+    return (
+      <div className="dropzone-standalone">
+        <div className="dropzone-inner" ref="select">
+          {this.attachmentInputs()}
+          To attach files drag &amp; drop here or <a href="javascript:void(0 );">select files
+          from your computer</a>...
         </div>
-      );
-    },
+      </div>
+    );
+  },
 
-    attachmentInputs: function() {
-      return this.state.attachments.map(function(attachmentId) {
-        return <input name={this.props.inputName} type="hidden" value={attachmentId} />
-      }.bind(this))
-    },
+  attachmentInputs: function() {
+    return this.state.attachments.map(function(attachmentId) {
+      return <input name={this.props.inputName} type="hidden" value={attachmentId} />
+    }.bind(this))
+  },
 
-    handleAccept: function(file, done) {
-      var attachment = new window.Attachment()
-      attachment.save({
-        name: file.name,
-        content_type: file.type,
-        size: file.size,
-      }, {
-        success: function(attachment) {
-          this.updateState({
-            attachments: { $push: [attachment.get('id')] }
-          })
-        }.bind(this)
-      })
-    },
+  handleAccept: AttachmentActionCreators.uploadAttachment,
 
-    updateState: function(update) {
-      this.setState(React.addons.update(this.state, update))
-    },
+  updateState: function(update) {
+    this.setState(React.addons.update(this.state, update))
+  },
 
-    handleSending: function(file, xhr, formData) {
-      for (var key in file.form) {
-        if (file.form.hasOwnProperty(key)) {
-          formData.append(key, file.form[key])
-        }
+  handleSending: function(file, xhr, formData) {
+    for (var key in file.form) {
+      if (file.form.hasOwnProperty(key)) {
+        formData.append(key, file.form[key])
       }
     }
-  })
-
-  if (typeof module !== 'undefined') {
-    module.exports = FormUploader
   }
+})
 
-  window.FormUploader = FormUploader
-})()
+if (typeof module !== 'undefined') {
+  module.exports = FormUploader
+}
+
+window.FormUploader = FormUploader
