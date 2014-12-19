41c8dc9f239483af5c771973cc16575b35c3cd1f
Charles Pletcher <cpletcher2@gmail.com>
2014-12-18 17:32:56 -0800

    Fix failing test

--- spec/javascripts/flux/stores/attachment_store_test.js
+++ spec/javascripts/flux/stores/attachment_store_test.js
@@ -9,7 +9,7 @@ describe('AttachmentStore', function(){
   beforeEach(function() {
     Dispatcher = require(pathToFile('dispatcher'));
     AttachmentStore = require(pathToFile('stores/attachment_store'));
-    callback = Dispatcher.register.mock.calls[4][0];
+    callback = Dispatcher.register.mock.calls[3][0];
   });
 
   describe('getAttachment()', function() {
