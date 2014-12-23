3f0ed8b9240e1839b6f6551963d66c158ccd5934
Dustin Hoffmann <dustintheweb@gmail.com>
2014-12-22 23:56:03 -0600

    fixed long url column break

--- app/assets/javascripts/components/news_feed/news_feed_item_bounty.js.jsx
+++ app/assets/javascripts/components/news_feed/news_feed_item_bounty.js.jsx
@@ -38,7 +38,7 @@ module.exports = React.createClass({
                 </span>
               </div>
             </div>
-            <div className="mt1 gray-darker fs4" key={'nfbi-body-' + bounty.id} onClick={this.showBounty}>
+            <div className="mt1 gray-darker fs4 break-word" key={'nfbi-body-' + bounty.id} onClick={this.showBounty}>
               <Markdown content={bounty.short_description} normalized={true} />
               {this.thumbnails()}
             </div>
