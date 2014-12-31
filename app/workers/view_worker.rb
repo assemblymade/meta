a6ca2b10940168f40ad8af8053f0b7963cd8775f
barisser <barisser@gmail.com>
2014-12-31 13:16:30 -0800

    added more tests to querymarks

--- app/workers/view_worker.rb
+++ app/workers/view_worker.rb
@@ -5,36 +5,10 @@ class ViewWorker
 
   include Sidekiq::Worker
   def perform(user_id, viewable_id, viewable_type)
-
-    if viewable_type == "Product"
-      viewable = Product.find(viewable_id)
-    elsif viewable_type == "Wip"
-      viewable = Wip.find(viewable_id)
+    if viewable_type == "Wip"
+      MakeMarks.new.mark_with_object_for_viewings(user_id, viewable_id, viewable_type, VIEW_BOUNTY_MARKING_WEIGHT)
+    elsif viewable_type == "Product"
+      MakeMarks.new.mark_with_object_for_viewings(user_id, viewable_id, viewable_type, VIEW_PRODUCT_MARKING_WEIGHT)
     end
-
-    applicable_viewings = Viewing.where(user_id: user_id, viewable_id: viewable_id)
-    previous_views = applicable_viewings.count
-
-    view_weight = 1.0# / (1.0 + previous_views)
-    puts "VIEWING #{viewable_id} #{viewable_type}"
-    if previous_views == 0
-      Viewing.create!({user_id: user_id, viewable_id: viewable_id, viewable_type: viewable_type, weight: view_weight})
-      if viewable_type == "Product"
-        AdjustMarkings.perform_async(user_id, viewable.id, viewable_type, VIEW_PRODUCT_MARKING_WEIGHT* Math.sqrt(view_weight))
-      elsif viewable_type == "Wip"
-        AdjustMarkings.perform_async(user_id, viewable.id, viewable_type, VIEW_BOUNTY_MARKING_WEIGHT* Math.sqrt(view_weight))
-      end
-
-    else
-      new_weight = applicable_viewings.first.weight + view_weight
-      applicable_viewings.first.update(weight: new_weight)
-      diff = Math.sqrt(new_weight) - Math.sqrt(view_weight)
-      if viewable_type == "Product"
-        AdjustMarkings.perform_async(user_id, viewable.id, viewable_type, VIEW_PRODUCT_MARKING_WEIGHT * diff)
-      elsif viewable_type == "Wip"
-        AdjustMarkings.perform_async(user_id, viewable.id, viewable_type, VIEW_BOUNTY_MARKING_WEIGHT * diff)
-      end
-    end
-
   end
 end
