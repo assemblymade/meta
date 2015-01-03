aab45d9668b48d3d49784bd9376c550f667e6c93
Charles Pletcher <cpletcher2@gmail.com>
2015-01-02 18:23:26 -0800

    Make pending tests pass

--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -5,7 +5,7 @@ describe AssetsController do
 
   describe '#create' do
     let(:current_user) { User.make! }
-    let(:product) { Product.make! }
+    let(:product) { Product.make!(user: current_user) }
     let(:attachment) { Attachment.make!(user: current_user) }
     let(:wip) { Task.make!(product: product, user: current_user) }
     let(:event) {
@@ -21,15 +21,30 @@ describe AssetsController do
     end
 
     it "transfers an event's attachments if passed an event_id" do
-      pending "Getting a 302, but not sure where it's coming from"
+      expect(product.assets.count).to eq(0)
+
+      post :create, product_id: product.slug, event_id: event.id
+
+      expect(product.assets.count).to eq(1)
+      expect(assigns(:asset).product).to eq(product)
     end
 
     it "transfers one attachment if passed an attachment_url" do
-      pending "Getting a 302, but not sure where it's coming from"
+      expect(product.assets.count).to eq(0)
+
+      post :create, product_id: product.slug, attachment_url: attachment.url
+
+      expect(product.assets.count).to eq(1)
+      expect(assigns(:asset).product).to eq(product)
     end
 
     it "creates an asset if passed an attachment_id and name" do
-      pending "Getting a 302, but not sure where it's coming from"
+      expect(product.assets.count).to eq(0)
+
+      post :create, product_id: product.slug, asset: { attachment_id: attachment.id, name: 'Caddy' }
+
+      expect(product.assets.count).to eq(1)
+      expect(assigns(:asset).product).to eq(product)
     end
   end
 end
