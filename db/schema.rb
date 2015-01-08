9004195463d4fb17674a17fbae7a37fbe1e52a77
Dave Newman <dave@snappyco.de>
2015-01-08 12:14:23 -0800

    schema file weirdness

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20150108190148) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "alpgsql"
+  enable_extension "plpgsql"
   enable_extension "hstore"
   enable_extension "pg_stat_statements"
   enable_extension "uuid-ossp"
