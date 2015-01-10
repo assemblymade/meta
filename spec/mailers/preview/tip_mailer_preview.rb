d7f39b9342b13168bd87e90c51b2200c94902a7e
Dave Newman <dave@snappyco.de>
2015-01-10 14:17:55 -0800

    Add email template for introduction tipping

--- spec/mailers/preview/tip_mailer_preview.rb
+++ spec/mailers/preview/tip_mailer_preview.rb
@@ -2,7 +2,7 @@ class TipMailerPreview < ActionMailer::Preview
 
   def tipped
     begin
-      tip = Tip.where(via_type: NewsFeedItemComment).sample
+      tip = Tip.uncached{ Tip.where(via_type: Activity).sample }
     end until !tip.via.nil?
 
     TipMailer.tipped(tip)
