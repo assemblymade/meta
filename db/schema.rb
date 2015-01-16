ce8b9f85481b35fc95b5a5f80a8fde2c97b97dbe
Charles Pletcher <cpletcher2@gmail.com>
2015-01-16 11:19:29 -0800

    Remove gif from reminder email

--- db/schema.rb
+++ db/schema.rb
@@ -297,16 +297,18 @@ ActiveRecord::Schema.define(version: 20150114214933) do
   add_index "hearts", ["user_id", "heartable_id"], name: "index_hearts_on_user_id_and_heartable_id", unique: true, using: :btree
 
   create_table "ideas", id: :uuid, force: :cascade do |t|
-    t.string   "slug",              limit: 255,                                 null: false
-    t.string   "name",              limit: 255,                                 null: false
+    t.string   "slug",               limit: 255,                                 null: false
+    t.string   "name",               limit: 255,                                 null: false
     t.text     "body"
-    t.uuid     "user_id",                                                       null: false
-    t.boolean  "claimed",                       default: false
+    t.uuid     "user_id",                                                        null: false
+    t.boolean  "claimed",                        default: false
     t.uuid     "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "score",                         default: 0.0
-    t.datetime "last_score_update",             default: '2013-06-06 00:00:00'
+    t.float    "score",                          default: 0.0
+    t.datetime "last_score_update",              default: '2013-06-06 00:00:00'
+    t.datetime "greenlit_at"
+    t.boolean  "founder_preference"
   end
 
   create_table "integrations", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
@@ -623,6 +625,7 @@ ActiveRecord::Schema.define(version: 20150114214933) do
     t.string   "state",                             limit: 255
     t.datetime "last_checked_btc"
     t.datetime "issued_coins"
+    t.text     "try_url"
   end
 
   add_index "products", ["authentication_token"], name: "index_products_on_authentication_token", unique: true, using: :btree
