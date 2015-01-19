96473c30c8cac39a1c6277782d7c81258c6f61f1
Chris Lloyd <christopher.lloyd@gmail.com>
2015-01-19 13:06:48 -0800

    Refactor nfi store hack.
    As per @vanstee and @pletcher

--- app/assets/javascripts/stores/news_feed_items_store.js
+++ app/assets/javascripts/stores/news_feed_items_store.js
@@ -50,23 +50,17 @@ var store = new NewsFeedItemsStore()
 var dataTag = document.getElementById('NewsFeedItemsStore')
 if (dataTag) {
   var data = JSON.parse(dataTag.innerHTML)
-
-  Dispatcher.dispatch({
+  var action = {
     type: ActionTypes.NEWS_FEED_ITEMS_RECEIVE,
     news_feed_items: data.news_feed_items
-  });
-
-  // This seems like a hack but the lack of `data.meta` was causing
-  // other errors downstream. Talk to @chrislloyd if you want an
-  // explanation.
-  if (data.meta && data.meta.pagination) {
-    Dispatcher.dispatch({
-      type: ActionTypes.NEWS_FEED_ITEMS_RECEIVE,
-      news_feed_items: data.news_feed_items,
-      page: data.meta.pagination.page,
-      pages: data.meta.pagination.pages
-    });
   }
+
+  if (data.meta && data.meta.pagination) {
+    action.page = data.meta.pagination.page
+    action.pages = data.meta.pagination.pages
+  }
+
+  Dispatcher.dispatch(action)
 }
 
 module.exports = store
