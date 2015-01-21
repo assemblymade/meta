441dbf89e114e2fffde9f09d887d6c1fb45d67ab
Chris Lloyd <christopher.lloyd@gmail.com>
2015-01-20 19:04:29 -0800

    Fix failing avatar test.

--- app/assets/javascripts/components/ui/avatar.js.jsx
+++ app/assets/javascripts/components/ui/avatar.js.jsx
@@ -1,49 +1,47 @@
-/** @jsx React.DOM */
+var Avatar = React.createClass({
+  propTypes: {
+    avatar_url: React.PropTypes.string,
+    size: React.PropTypes.oneOfType([
+            React.PropTypes.number,
+            React.PropTypes.string
+          ]),
+    user: React.PropTypes.shape({
+            username: React.PropTypes.string,
+            avatar_url: React.PropTypes.string
+          }),
+    style: React.PropTypes.object
+  },
 
-(function() {
-  var Avatar = React.createClass({
-    propTypes: {
-      avatar_url: React.PropTypes.string,
-      size: React.PropTypes.oneOfType([ React.PropTypes.number, React.PropTypes.string ]),
-      user: React.PropTypes.object,
-      style: React.PropTypes.object
-    },
-
-    getDefaultProps: function() {
-      return {
-        size: 24,
-        style: {}
-      };
-    },
-
-    render: function() {
-      var size = this.props.size.toString()
-      var username = this.props.user.username
-
-      return <img className="avatar"
-          src={this.avatarUrl()}
-          height={size}
-          width={size}
-          style={this.props.style}
-          alt={this.props.user.username} />;
-    },
-
-    avatarUrl: function() {
-      if (this.props.avatar_url) {
-        return this.props.avatar_url
-      }
-
-      if (!this.props.user || !this.props.user.avatar_url || this.props.alwaysDefault) {
-        return '/assets/avatars/default.png';
-      }
-
-      return this.props.user.avatar_url;
+  getDefaultProps: function() {
+    return {
+      size: 24,
+      style: {}
     }
-  });
+  },
 
-  if (typeof module !== 'undefined') {
-    module.exports = Avatar;
+  render: function() {
+    var size = this.props.size.toString()
+    var username = this.props.user.username
+
+    return <img className="avatar"
+        src={this.avatarUrl()}
+        height={size}
+        width={size}
+        style={this.props.style}
+        alt={username} />
+  },
+
+  avatarUrl: function() {
+    if (this.props.avatar_url) {
+      return this.props.avatar_url
+    }
+
+    if (!this.props.user || !this.props.user.avatar_url || this.props.alwaysDefault) {
+      return '/assets/avatars/default.png'
+    }
+
+    return this.props.user.avatar_url
   }
+})
 
-  window.Avatar = Avatar;
-})();
+module.exports = window.Avatar = Avatar
