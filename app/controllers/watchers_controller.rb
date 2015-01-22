9f65929e82f53b072ad570c6faed51a02d513bb9
Chris Lloyd <christopher.lloyd@gmail.com>
2015-01-21 17:28:07 -0800

    :bomb: watch out here comes @chrislloyd, dead code destroyer :hocho: :feels good:

--- app/controllers/watchers_controller.rb
+++
@@ -1,9 +0,0 @@
-class WatchersController < ProductController
-  respond_to :json, :html
-
-  def index
-    set_product
-    @watchers = @product.watchers.order('lower(username)')
-    respond_with(@watchers)
-  end
-end
