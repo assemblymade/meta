ce125f23b8dd77e3c669196e381b375d5a7eb0f6
Charles Pletcher <cpletcher2@gmail.com>
2015-01-22 10:18:02 -0800

    Attempt to fix the build

--- db/migrate/20150106200506_add_greenlit_at_to_ideas.rb
+++ db/migrate/20150106200506_add_greenlit_at_to_ideas.rb
@@ -1,10 +1,6 @@
 class AddGreenlitAtToIdeas < ActiveRecord::Migration
   def up
     add_column :ideas, :greenlit_at, :datetime
-
-    Idea.find_each do |idea|
-      idea.greenlight! if idea.should_greenlight?
-    end
   end
 
   def down
