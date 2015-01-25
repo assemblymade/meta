f9860168d8d16062aad651bcd664fbd5a011e833
Chris Lloyd <christopher.lloyd@gmail.com>
2015-01-25 11:52:22 -0800

    Only show code climate warnings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,10 @@ end
 
 Capybara.javascript_driver = :poltergeist
 
+CodeClimate::TestReporter.configure do |config|
+  config.logger.level = Logger::WARN
+end
+
 CodeClimate::TestReporter.start
 
 SimpleCov.start
