590ec26a328255f76c5abd00e7ca1ee23944adbd
Ben <hello@benshyong.com>
2015-01-26 15:48:50 -0800

    rake db

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20150126214402) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "uuid-ossp"
   enable_extension "plpgsql"
   enable_extension "hstore"
   enable_extension "pg_stat_statements"
-  enable_extension "uuid-ossp"
 
   create_table "activities", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "type",         limit: 255
@@ -1106,6 +1106,7 @@ ActiveRecord::Schema.define(version: 20150126214402) do
     t.datetime "created_at"
   end
 
+  add_foreign_key "markings", "marks"
   add_foreign_key "news_feed_item_comments", "news_feed_items"
   add_foreign_key "showcase_entries", "products"
   add_foreign_key "showcase_entries", "showcases"
