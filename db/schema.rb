1053d2893a15cd54581e04bb2dc02000dfcfc177
Chris Lloyd <christopher.lloyd@gmail.com>
2015-01-27 15:34:23 -0800

    Showcase pages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126214402) do
+ActiveRecord::Schema.define(version: 20150127003026) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "uuid-ossp"
   enable_extension "plpgsql"
   enable_extension "hstore"
   enable_extension "pg_stat_statements"
-  enable_extension "uuid-ossp"
 
   create_table "activities", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "type",         limit: 255
@@ -699,6 +699,7 @@ ActiveRecord::Schema.define(version: 20150126214402) do
     t.datetime "created_at", null: false
     t.string   "slug",       null: false
     t.datetime "ended_at"
+    t.string   "background"
   end
 
   add_index "showcases", ["ended_at"], name: "index_showcases_on_ended_at", using: :btree
