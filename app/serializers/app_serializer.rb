9980b497383023f4bd2b21ddefb4a4827b263720
Dave Newman <dave@snappyco.de>
2015-01-28 15:53:55 -0800

    fix broken default logo on product page

--- app/serializers/app_serializer.rb
+++ app/serializers/app_serializer.rb
@@ -2,7 +2,7 @@ class AppSerializer < ApplicationSerializer
   attributes :name, :pitch, :slug, :logo_url, :try_url, :search_tags
 
   def logo_url
-    object.logo_url == Product::DEFAULT_IMAGE_PATH ? File.join(root_url, Product::DEFAULT_IMAGE_PATH) : object.logo_url
+    object.full_logo_url
   end
 
   def search_tags
