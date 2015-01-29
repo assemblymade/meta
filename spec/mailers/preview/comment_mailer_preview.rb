dc5ef05a56ace5275019c5f0d5a50f40118cd37b
Dave Newman <dave@snappyco.de>
2015-01-29 11:31:10 -0800

    fix new comment mailer for ideas

--- spec/mailers/preview/comment_mailer_preview.rb
+++ spec/mailers/preview/comment_mailer_preview.rb
@@ -5,7 +5,9 @@ class CommentMailerPreview < ActionMailer::Preview
   end
 
   def new_comment
-    comment = NewsFeedItemComment.where("body ilike '%http%'").sample
+    # comment = NewsFeedItemComment.where("body ilike '%http%'").sample
+    comment = NewsFeedItemComment.joins(:news_feed_item).
+                where(news_feed_items: { target_type: Idea}).sample
     CommentMailer.new_comment(comment.news_feed_item.source_id, comment.id)
   end
 end
