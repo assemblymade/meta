c87bf950fcf5c283bc1eb810225de0c45a5c825c
Chris Lloyd <christopher.lloyd@gmail.com>
2015-02-01 20:25:55 -0800

    Change method signature of Discussion to accept children.

--- app/assets/javascripts/components/ui/discussion.js.jsx
+++ app/assets/javascripts/components/ui/discussion.js.jsx
@@ -4,8 +4,7 @@ var NewFeedItemComments = require('../news_feed/news_feed_item_comments.js.jsx')
 var Discussion = React.createClass({
 
   propTypes: {
-    newsFeedItem: React.PropTypes.object.isRequired,
-    target: React.PropTypes.element.isRequired
+    newsFeedItem: React.PropTypes.object.isRequired
   },
 
   render: function() {
@@ -13,7 +12,7 @@ var Discussion = React.createClass({
 
     return (
       <Tile>
-        {this.props.target}
+        {this.props.children}
         <div className="border-top border-gray-5">
           <div className="px2 _mq-600_px4">
             <NewsFeedItemComments commentable={true} item={item} showAllComments={true} />
