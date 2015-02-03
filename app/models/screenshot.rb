25a4ee8945b37fbd2b4fd8be1baa22d5c4b59048
Charles Pletcher <cpletcher2@gmail.com>
2015-02-02 17:20:38 -0800

    Fix styling of images in carousel, mysteriously fix AWS S3 403

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,8 +1,7 @@
 class Screenshot < ActiveRecord::Base
   belongs_to :asset
 
-  validates :position, presence: true,
-                       uniqueness: { scope: :asset }
+  validates :position, presence: true
 
   def url
     asset.url
