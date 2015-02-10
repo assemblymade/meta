fc2e6057749baec6737f715666d52529665be3cf
vanstee <patrick@vanstee.me>
2015-02-10 15:23:23 -0500

    Get the job working for non-discussion models

--- app/workers/indexer.rb
+++ app/workers/indexer.rb
@@ -7,7 +7,7 @@ class Indexer
     case operation.to_s
       when /index/
         record = klass.find(record_id)
-        return if record.chat?
+        return if record.respond_to?(:chat?) && record.chat?
         Elasticsearch::Model.client.index  index: klass.index_name, type: klass.name.downcase, id: record.id, body: record.as_indexed_json
       when /delete/
         Elasticsearch::Model.client.delete index: klass.index_name, type: klass.name.downcase, id: record_id
