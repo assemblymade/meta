444db736130d84372a22e183869c0fbb92adf7a7
Dave Newman <dave@snappyco.de>
2015-03-02 17:12:53 -0800

    Fix metrics controller spec

--- spec/controllers/metrics_controller_spec.rb
+++ spec/controllers/metrics_controller_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe MetricsController do
   let(:product) { Product.make! }
-
+  let(:user) { User.make!(is_staff: true)}
 
   describe "#snippet" do
     it "show snippet" do
+      sign_in user
       get :snippet, product_id: product.slug, format: :json
 
       expect(JSON.parse(response.body)['analytics_snippet']).to be_blank
