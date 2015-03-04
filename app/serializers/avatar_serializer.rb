157193c8d97a725c103e9c9a3075bc482bbe4bde
Chris Lloyd <christopher.lloyd@gmail.com>
2015-03-04 15:04:32 -0800

    Add links to core team avatars.

--- app/serializers/avatar_serializer.rb
+++ app/serializers/avatar_serializer.rb
@@ -1,7 +1,13 @@
 class AvatarSerializer < ApplicationSerializer
   attributes :username, :avatar_url
+  attributes :url
 
   def avatar_url
     object.avatar.url(288).to_s
   end
+
+  def url
+    user_path(object)
+  end
+  
 end
