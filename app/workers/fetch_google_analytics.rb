80754d8f424d1150f402ccff414d281181a5d8b9
Dave Newman <dave@snappyco.de>
2015-03-07 10:42:20 -0800

    Update GA fetcher to support multiple accounts with the same name

--- app/workers/fetch_google_analytics.rb
+++ app/workers/fetch_google_analytics.rb
@@ -1,40 +1,23 @@
 require 'google/api_client'
 
 class FetchGoogleAnalytics
+  attr_reader :i
 
   def perform(integration_id)
-    i = Integration.find(integration_id)
+    @i = Integration.find(integration_id)
 
-    client = Google::APIClient.new(
-      application_name: 'Assembly',
-      application_version: '1'
-    )
-    client.authorization.client_id = ENV['GOOGLE_CLIENT_ID']
-    client.authorization.client_secret = ENV['GOOGLE_CLIENT_SECRET']
-    client.authorization.refresh_token = i.refresh_token
-    client.authorization.grant_type = 'refresh_token'
-    client.authorization.fetch_access_token!
+    Rails.logger.info "integration=#{i.id} product=#{i.product.slug} config=#{i.config.inspect}"
 
-    analytics = client.discovered_api('analytics', 'v3')
-
-    result = client.execute(analytics.management.accounts.list)
-    account = result.data.items.count == 1 ? result.data.items.first : result.data.items.find{|o| o.name == i.config['account_name'] }
-    if account.nil?
-      raise "not found (account:#{i.config['account_name']})"
+    monthly_metrics.each do |year, month, visits|
+      date = Date.parse([year, month, 1].join('-'))
+      m = i.product.monthly_metrics.find_or_initialize_by(date: date)
+      m.update!(
+        ga_uniques: visits.to_i,
+      )
     end
+  end
 
-    result = client.execute(analytics.management.webproperties.list, accountId: account.id)
-    property = result.data.items.count == 1 ? result.data.items.first : result.data.items.find{|o| o.name == i.config['property_name'] }
-    if account.nil?
-      raise "not found (property:#{i.config['property_name']})"
-    end
-
-    result = client.execute(analytics.management.profiles.list, accountId: account.id, webPropertyId: property.id)
-    profile = result.data.items.count == 1 ? result.data.items.first : result.data.items.find{|o| o.name == i.config['profile_name'] }
-    if account.nil?
-      raise "not found (profile:#{i.config['profile_name']})"
-    end
-
+  def monthly_metrics
     result = client.execute(
       api_method: analytics.data.ga.get,
       parameters: {
@@ -47,12 +30,69 @@ class FetchGoogleAnalytics
       }
     )
 
-    result.data.rows.each do |year, month, visits|
-      date = Date.parse([year, month, 1].join('-'))
-      m = i.product.monthly_metrics.find_or_initialize_by(date: date)
-      m.update!(
-        ga_uniques: visits.to_i,
+    result.data.rows
+  end
+
+  def profile
+    @profile ||= begin
+      result = client.execute(analytics.management.accounts.list)
+      accounts = result.data.items.count == 1 ? [result.data.items.first] : result.data.items.select{|o| o.name == i.config['account_name'] }
+      if accounts.empty?
+        raise "not found (account:#{i.config['account_name']}) #{result.data.items.map(&:name)}"
+      end
+
+      Rails.logger.info "integration=#{i.id} product=#{i.product.slug} config=#{i.config.inspect} accounts=#{accounts.map(&:name)}"
+
+      account = nil
+      property = nil
+      accounts.find do |a|
+        result = client.execute(analytics.management.webproperties.list, accountId: a.id)
+        property = result.data.items.count == 1 ? result.data.items.first : result.data.items.find{|o| o.name == i.config['property_name'] }
+        puts "property #{property.try(:name)}"
+        if property
+          account = a
+        end
+      end
+      if property.nil?
+        raise "not found (property:#{i.config['property_name']}) #{result.data.items.map(&:name)}"
+      end
+
+      Rails.logger.info "integration=#{i.id} product=#{i.product.slug} config=#{i.config.inspect} account=#{account.name} property=#{property.name}"
+
+      result = client.execute(analytics.management.profiles.list, accountId: account.id, webPropertyId: property.id)
+      profile = result.data.items.count == 1 ? result.data.items.first : result.data.items.find{|o| o.name == i.config['profile_name'] }
+      if profile.nil?
+        raise "not found (profile:#{i.config['profile_name']}) #{result.data.items.map(&:name)}"
+      end
+
+      Rails.logger.info "integration=#{i.id} product=#{i.product.slug} config=#{i.config.inspect} account=#{} property=#{property.name}"
+
+      profile
+    end
+  end
+
+  def analytics
+    @analytics ||= client.discovered_api('analytics', 'v3')
+  end
+
+  def client
+    @client ||= begin
+      client = Google::APIClient.new(
+        application_name: 'Assembly',
+        application_version: '1'
       )
+      client.authorization.client_id = ENV['GOOGLE_CLIENT_ID']
+      client.authorization.client_secret = ENV['GOOGLE_CLIENT_SECRET']
+
+      if i.refresh_token
+        client.authorization.refresh_token = i.refresh_token
+        client.authorization.grant_type = 'refresh_token'
+        client.authorization.fetch_access_token!
+      else
+        client.authorization.access_token = i.access_token
+        client.authorization.grant_type = 'authorization_code'
+      end
+      client
     end
   end
 end
