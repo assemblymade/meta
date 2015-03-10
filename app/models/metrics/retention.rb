4e009c9a0f5590a4990ed8b97af7696a3349ec2b
Dave Newman <dave@snappyco.de>
2015-03-10 10:43:53 -0700

    Fix retention metric to calculate average

--- app/models/metrics/retention.rb
+++ app/models/metrics/retention.rb
@@ -1,18 +1,20 @@
 module Metrics
   class Retention < KPI
     def average
-      cohorts = MixpanelClient.new.request(
+      result = MixpanelClient.new.request(
         'retention',
         from_date: 5.weeks.ago.strftime('%Y-%m-%d'),
-        to_date: Time.now.strftime('%Y-%m-%d'),
+        to_date: 2.weeks.ago.end_of_week.strftime('%Y-%m-%d'),
         retention_type: 'birth',
         born_event: 'user.created',
         unit: 'week'
       )
 
-      cohorts.
+      cohorts = result.
         map{|date, vals| {date => (vals['counts'][1].to_i / vals['first'].to_f) } }.
         sort_by{|e| Date.parse(e.keys.first) }
+
+      cohorts.map{|e| e.values.first}.reduce(:+) / cohorts.size.to_f
     end
   end
 end
