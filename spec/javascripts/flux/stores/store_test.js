18765e950ae84176e1f4c6b9c5adaa0ed629318d
Dave Newman <dave@snappyco.de>
2015-03-12 11:27:19 -0700

    Remove jest from rake to fix travis

--- spec/javascripts/flux/stores/store_test.js
+++ spec/javascripts/flux/stores/store_test.js
@@ -1,52 +1,52 @@
-// jest.dontMock(pathToFile('stores/store'));
-//
-// describe('Store', function() {
-//   var events = require('events')
-//
-//   // console.log(events)
-//
-//   var EventEmitter = events.EventEmitter;
-//
-//   console.log(EventEmitter.prototype.emit)
-//
-//   var Store = require(pathToFile('stores/store'));
-//
-//   it('instantiates a new store that has EventEmitter\'s prototype', function() {
-//     expect(Store.emitChange).toBeDefined();
-//     expect(Store.addChangeListener).toBeDefined();
-//     expect(Store.removeChangeListener).toBeDefined();
-//     expect(Store.emit).toBeDefined();
-//   });
-//
-//   describe('emitChange()', function() {
-//     it('emits changes', function() {
-//       Store.emitChange();
-//
-//       expect(EventEmitter.prototype.emit).toBeCalled();
-//     });
-//   });
-//
-//   describe('addChangeListener()', function() {
-//     it('adds a change listener', function() {
-//       var mock = jest.genMockFn();
-//
-//       Store.addChangeListener(mock);
-//
-//       expect(EventEmitter.prototype.on).toBeCalled();
-//     });
-//   });
-//
-//   describe('removeChangeListener', function() {
-//     it('removes a listener', function() {
-//       var mock = jest.genMockFn();
-//
-//       Store.addChangeListener(mock);
-//
-//       expect(EventEmitter.prototype.on).toBeCalled();
-//
-//       Store.removeChangeListener(mock);
-//
-//       expect(EventEmitter.prototype.removeListener).toBeCalled();
-//     });
-//   });
-// });
+jest.dontMock(pathToFile('stores/store'));
+
+describe('Store', function() {
+  var events = require('events')
+
+  // console.log(events)
+
+  var EventEmitter = events.EventEmitter;
+
+  console.log(EventEmitter.prototype.emit)
+
+  var Store = require(pathToFile('stores/store'));
+
+  it('instantiates a new store that has EventEmitter\'s prototype', function() {
+    expect(Store.emitChange).toBeDefined();
+    expect(Store.addChangeListener).toBeDefined();
+    expect(Store.removeChangeListener).toBeDefined();
+    expect(Store.emit).toBeDefined();
+  });
+
+  describe('emitChange()', function() {
+    it('emits changes', function() {
+      Store.emitChange();
+
+      expect(EventEmitter.prototype.emit).toBeCalled();
+    });
+  });
+
+  describe('addChangeListener()', function() {
+    it('adds a change listener', function() {
+      var mock = jest.genMockFn();
+
+      Store.addChangeListener(mock);
+
+      expect(EventEmitter.prototype.on).toBeCalled();
+    });
+  });
+
+  describe('removeChangeListener', function() {
+    it('removes a listener', function() {
+      var mock = jest.genMockFn();
+
+      Store.addChangeListener(mock);
+
+      expect(EventEmitter.prototype.on).toBeCalled();
+
+      Store.removeChangeListener(mock);
+
+      expect(EventEmitter.prototype.removeListener).toBeCalled();
+    });
+  });
+});
