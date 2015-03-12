8e0dc823e85c488f5087316cbb3c790c14688c87
Austin Smith <awwstn@gmail.com>
2015-03-12 13:50:12 -0700

    reorganizing guides

--- app/models/guides_group.rb
+++ app/models/guides_group.rb
@@ -1,18 +1,18 @@
 class GuidesGroup < StaticContentGroup
   GROUPS = [
-    ['platform',    'The Assembly platform'],
-    ['getting-started',    'Getting started'],
-    ['building-products',    'Building products'],
-    ['accepting-payments',    'Accepting payments'],
-    ['project-management', 'Project management'],
-    ['leading', 'Leading a product'],
+    ['platform',  'The Assembly platform'],
+    ['product-management', 'Product management'],
+    ['tips', 'Tips for success'],
+    ['accepting-payments',  'Accepting payments'],
+    ['getting-paid',  'Getting paid'],
+    ['domains',  'Domains, hosting, and repos'],
+    ['ownership', 'Managing Ownership']
     ['starting-ios', 'Starting an iOS app'],
     ['transferring-ios', 'Transferring an iOS app'],
     ['starting-android', 'Starting an Android app'],
     ['transferring-android', 'Transferring an Android app'],
     ['starting-web', 'Starting a Web app'],
     ['transferring-web', 'Transferring a Web app'],
-    ['ownership', 'Managing Ownership']
   ]
 
   def self.all
