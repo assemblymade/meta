b5829271d70b1b4df8bf39469c1f7bb014d5bd90
Dave Newman <dave@snappyco.de>
2015-03-13 10:02:30 -0700

    better coin caching of awards

--- db/migrate/20150312231554_update_awards_to_cache_coins.rb
+++ db/migrate/20150312231554_update_awards_to_cache_coins.rb
@@ -1,13 +1,18 @@
 class UpdateAwardsToCacheCoins < ActiveRecord::Migration
-  def change
-    Award.where(cents: nil).includes(:wip).find_each do |a|
+  def up
+    Award.where(cents: [nil, 0]).includes(:wip).find_each do |a|
       coins = 0
       if a.wip.offers.count == 0
         a.update!(cents: 0)
       else
         entries = TransactionLogEntry.
-          where(product_id: a.wip.product_id, wallet_id: a.winner_id).
-          where('created_at > ? and created_at < ?', a.created_at - 10, a.created_at + 10)
+          where(work_id: a.wip.id, wallet_id: a.winner_id)
+
+        if !entries.any?
+          entries = TransactionLogEntry.
+            where(product_id: a.wip.product_id, wallet_id: a.winner_id).
+            where('created_at > ? and created_at < ?', a.created_at - 10, a.created_at + 10)
+        end
 
         if entries.any?
           a.update!(cents: entries.sum(:cents))
