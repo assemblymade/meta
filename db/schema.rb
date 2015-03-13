df5b0e707f21f29b73794146c94abf3c48eb9c5a
Ben <hello@benshyong.com>
2015-03-13 11:57:37 -0700

    merge master

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20150312205251) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "uuid-ossp"
-  enable_extension "hstore"
   enable_extension "plpgsql"
+  enable_extension "hstore"
   enable_extension "pg_stat_statements"
+  enable_extension "uuid-ossp"
 
   create_table "activities", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "type",         limit: 255
@@ -412,10 +412,9 @@ ActiveRecord::Schema.define(version: 20150312205251) do
   create_table "leader_positions", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "leader_type"
     t.integer  "rank"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.uuid     "user_id"
-    t.string   "leader_category"
   end
 
   create_table "mark_clusters", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
@@ -569,46 +568,6 @@ ActiveRecord::Schema.define(version: 20150312205251) do
     t.datetime "updated_at"
     t.datetime "cancelled_at"
   end
-
-  create_table "oauth_access_grants", force: :cascade do |t|
-    t.uuid     "resource_owner_id", null: false
-    t.integer  "application_id",    null: false
-    t.string   "token",             null: false
-    t.integer  "expires_in",        null: false
-    t.text     "redirect_uri",      null: false
-    t.datetime "created_at",        null: false
-    t.datetime "revoked_at"
-    t.string   "scopes"
-  end
-
-  add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true, using: :btree
-
-  create_table "oauth_access_tokens", force: :cascade do |t|
-    t.uuid     "resource_owner_id"
-    t.integer  "application_id"
-    t.string   "token",             null: false
-    t.string   "refresh_token"
-    t.integer  "expires_in"
-    t.datetime "revoked_at"
-    t.datetime "created_at",        null: false
-    t.string   "scopes"
-  end
-
-  add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true, using: :btree
-  add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id", using: :btree
-  add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true, using: :btree
-
-  create_table "oauth_applications", force: :cascade do |t|
-    t.string   "name",                      null: false
-    t.string   "uid",                       null: false
-    t.string   "secret",                    null: false
-    t.text     "redirect_uri",              null: false
-    t.string   "scopes",       default: "", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
   create_table "offers", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.uuid     "bounty_id",  null: false
@@ -855,7 +814,6 @@ ActiveRecord::Schema.define(version: 20150312205251) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
-    t.integer  "order"
   end
 
   create_table "status_messages", id: :uuid, force: :cascade do |t|
@@ -1115,7 +1073,6 @@ ActiveRecord::Schema.define(version: 20150312205251) do
     t.uuid     "user_cluster_id"
     t.datetime "flagged_at"
     t.datetime "showcase_banner_dismissed_at"
-    t.uuid     "leader_position_id"
     t.datetime "coin_callout_viewed_at"
     t.integer  "hearts_received",                               default: 0,       null: false
   end
