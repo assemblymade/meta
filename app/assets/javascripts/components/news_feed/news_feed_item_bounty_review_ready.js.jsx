d43e8579b0611f047bf5f8512fb2bc152a60b3aa
Chris Lloyd <christopher.lloyd@gmail.com>
2015-04-07 11:18:49 -0700

    Bring award and close buttons in line with the other style.

--- app/assets/javascripts/components/news_feed/news_feed_item_bounty_review_ready.js.jsx
+++ app/assets/javascripts/components/news_feed/news_feed_item_bounty_review_ready.js.jsx
@@ -1,7 +1,9 @@
-var NewsFeedItemEvent = require('./news_feed_item_event.js.jsx');
+'use strict'
 
-module.exports = React.createClass({
-  displayName: 'NewsFeedItemBountyReviewReady',
+import NewsFeedItemEvent from './news_feed_item_event.js.jsx';
+import List from '../ui/list.js.jsx';
+
+const NewsFeedItemBountyReviewReady = React.createClass({
   propTypes: {
     actor: React.PropTypes.object.isRequired,
     award_url: React.PropTypes.string,
@@ -9,42 +11,37 @@ module.exports = React.createClass({
   },
 
   render: function() {
-    var actor = this.props.actor;
+    const {actor, id} = this.props
+    const awardUrl = this.props.award_url
 
     return (
       <NewsFeedItemEvent>
-        {this.renderAwardButtons()}
-
-        <div>
-          <a href={actor.url}>{actor.username}</a>
-          {' '} submitted work for review
+        <div className="mb1">
+          <a className="bold black" href={actor.url}>
+            {actor.username}
+          </a> submitted work to be reviewed by the core team
         </div>
-      </NewsFeedItemEvent>
-    );
-  },
-
-  renderAwardButtons: function() {
-    var actor = this.props.actor;
-    var awardUrl = this.props.award_url;
-    var id = this.props.id;
-
-    if (awardUrl) {
-      return (
-        <div className="btn-group right">
-          <a className="btn btn-default btn-xs"
+        <List type="piped">
+          <List.Item>
+            <a className="gray-2 black-hover"
               href={awardUrl + '?event_id=' + id}
-              data-method="patch"
-              data-confirm={'Are you sure you want to award this task to @' + actor.username + '?'}>
-            Award
-          </a>
-          <a className="btn btn-primary btn-xs"
-              href={awardUrl + '?event_id=' + id + '&close=true'}
-              data-method="patch"
-              data-confirm={'Are you sure you want to award this task to @' + actor.username + '?'}>
-            Award and close
-          </a>
-        </div>
-      );
-    }
-  }
-});
+               data-method="patch"
+               data-confirm={'Are you sure you want to award this task to @' + actor.username + '?'}>
+              Award
+            </a>
+          </List.Item>
+          <List.Item>
+            <a className="gray-2 black-hover"
+               href={awardUrl + '?event_id=' + id + '&close=true'}
+               data-method="patch"
+               data-confirm={'Are you sure you want to award this task to @' + actor.username + '?'}>
+              Award and close
+            </a>
+          </List.Item>
+        </List>
+      </NewsFeedItemEvent>
+    )
+  },
+})
+
+export default NewsFeedItemBountyReviewReady
