72fb657685e8a54488d637c2203075fd5c2c1b97
barisser <barisser@gmail.com>
2015-04-07 16:00:43 -0700

    dont do transaction log entries immediately upon greenlighting, save for rake task when we have asset address

--- app/assets/javascripts/components/products/product_partners.js.jsx
+++ app/assets/javascripts/components/products/product_partners.js.jsx
@@ -7,6 +7,7 @@ const Markdown = require('../markdown.js.jsx')
 import ProductHeader from '../products/product_header.js.jsx'
 import ProductStore from '../../stores/product_store'
 import PartnersStore from '../../stores/partners_store'
+import Button from '../ui/button.js.jsx';
 
 function _parseDate(date) {
   var parsedDate = new Date(date);
@@ -18,7 +19,8 @@ const ProductPartners = React.createClass({
 
   getInitialState() {
     return {
-      partners: PartnersStore.get(ProductStore.getId())
+      partners: PartnersStore.get(ProductStore.getId()),
+      coinprism_url: ProductStore.getCoinPrismUrl()
     }
   },
 
@@ -64,6 +66,14 @@ const ProductPartners = React.createClass({
           <td className="right-align">
             {formattedCoins}
           </td>
+
+          <td>
+            <a href={partner.coinprism_url}>
+              <div className="">
+                {partner.wallet_public_address}
+              </div>
+            </a>
+          </td>
         </tr>
       )
     })
@@ -72,6 +82,7 @@ const ProductPartners = React.createClass({
         <div className="container mt3">
           <Tile>
           <div className="p4">
+            <Button action={this.state.coinprism_url}>See it on the Blockchain</Button>
             <div className="table-responsive">
               <table className="table">
                 <thead>
@@ -79,6 +90,7 @@ const ProductPartners = React.createClass({
                     <th>Partner</th>
                     <th className="right-align">Coins</th>
                     <th className="right-align">Ownership</th>
+                    <th>Public Address</th>
                   </tr>
                 </thead>
                 <tbody>
