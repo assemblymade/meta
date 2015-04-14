94fb5c17c9c08e999df7fac51a9925f321b36dca
Charles Pletcher <cpletcher2@gmail.com>
2015-04-14 10:25:17 -0700

    Guard against null payloads in SSOController#sign

--- app/controllers/single_sign_on_controller.rb
+++ app/controllers/single_sign_on_controller.rb
@@ -42,8 +42,13 @@ class SingleSignOnController < ApplicationController
   end
 
   def sign(payload)
-    digest = OpenSSL::Digest.new('sha256')
-    OpenSSL::HMAC.hexdigest(digest, ENV["LANDLINE_SECRET"], payload)
+    return false unless payload
+
+    OpenSSL::HMAC.hexdigest(
+      OpenSSL::Digest.new('sha256'),
+      ENV["LANDLINE_SECRET"],
+      payload
+    )
   end
 
   def set_access_control_headers
