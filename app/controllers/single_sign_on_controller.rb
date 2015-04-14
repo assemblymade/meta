0a403d8a4fd5142ee95df58271a1af3e0eca96e7
Charles Pletcher <cpletcher2@gmail.com>
2015-04-14 10:03:13 -0700

    Don't show Landline for beta users

--- app/controllers/single_sign_on_controller.rb
+++ app/controllers/single_sign_on_controller.rb
@@ -1,6 +1,5 @@
 class SingleSignOnController < ApplicationController
   after_filter :set_access_control_headers
-  # before_action :authenticate_user!
 
   def sso
     return render nothing: true, status: 401 unless sign(params[:payload]) == params[:sig]
