ac4b86f6be8ad65084b585bb170d74af03aadf44
Austin Smith <awwstn@gmail.com>
2015-04-14 15:37:22 -0700

    change 'import product' link

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -32,7 +32,7 @@ var Navbar = React.createClass({
       userDropdownMenu = (
         <DropdownMenu position="right" key="user dropdown menu">
           <DropdownMenu.Item label="Start an idea" icon="lightbulb-o" action='/ideas/new' />
-          <DropdownMenu.Item label="Import product" icon="building" action='/new' />
+          <DropdownMenu.Item label="Import product" icon="building" action='/import' />
 
           <DropdownMenu.Divider />
 
