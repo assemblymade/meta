a09ba3a3410ed35fcd23a35f3b72dfa4f611992f
lachlanjc <hello@lachlanjc.me>
2015-04-30 21:20:30 -0400

    Revert "Build <Sheet />"
    This reverts commit be58b2e05cf61e0789a8097ba16ee03351eb9b5f.

--- app/assets/javascripts/components/ui/tile.js.jsx
+++ app/assets/javascripts/components/ui/tile.js.jsx
@@ -1,13 +1,6 @@
 var Tile = React.createClass({
-  getDefaultProps: function() {
-    return {
-      padding: 0,
-    };
-  },
-
   render: function() {
-    var padding = (this.props.padding === 0) ? null : " p" + this.props.padding;
-    return <div className={"bg-white rounded shadow border border-gray-5" + padding}>
+    return <div className="bg-white rounded shadow border border-gray-5">
       {this.props.children}
     </div>
   }
