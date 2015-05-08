c26da35c5a4b06b18c0ac698dc246cbde64134a4
lachlanjc <hello@lachlanjc.me>
2015-05-07 21:36:19 -0400

    Fixes for the 👀

--- app/assets/javascripts/components/ideas/ideas_new.js.jsx
+++ app/assets/javascripts/components/ideas/ideas_new.js.jsx
@@ -34,13 +34,13 @@ let IdeasNew = React.createClass({
 
   render() {
     return (
-      <div className="sm-col-11 md-col-6 mx-auto mt4 mb4">
+      <div className="sm-col-11 md-col-8 mx-auto mt3 mb4">
         <Tile>
           <div className="mt3 mb4 px4">
-            <img src="https://d1015h9unskp4y.cloudfront.net/attachments/da827015-7f37-4fdb-80d0-b8d68ae71f32/asm_mountain_360.png" />
+            <img className="mx-auto block" src="https://d1015h9unskp4y.cloudfront.net/attachments/da827015-7f37-4fdb-80d0-b8d68ae71f32/asm_mountain_360.png" />
 
             <h3 className="mb1 center">Turn your idea into a great product</h3>
-            <h4 className="mt0 mb0 regular gray-2 center">The community collaborates to build the best ideas into products.</h4>
+            <h4 className="mt0 mb3 regular gray-2 center">The community collaborates to build the best ideas into products.</h4>
 
             <IdeaForm />
           </div>
