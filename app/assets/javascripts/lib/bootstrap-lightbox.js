c3d527fb3a1559d560598da35ad821addb589868
Lachlan Campbell <lachlanjc@users.noreply.github.com>
2015-05-15 08:14:58 -0400

    Cleanup for Hound

--- app/assets/javascripts/lib/bootstrap-lightbox.js
+++ app/assets/javascripts/lib/bootstrap-lightbox.js
@@ -100,7 +100,7 @@
 			.attr('aria-hidden', true)
 			.off('click.dismiss.lightbox');
 
-		$.support.transition && this.$element.hasClass('muted') ?
+		$.support.transition && this.$element.hasClass("muted") ?
 			this.$element
 				.one($.support.transition.end, $.proxy(this.hideModal(slide), this))
 				.emulateTransitionEnd(300) :
@@ -150,13 +150,12 @@
 	Lightbox.prototype.backdrop = function (callback)
 	{
 		var that    = this
-		var animate = this.$element.hasClass('muted') ? 'muted' : ''
+		var animate = this.$element.hasClass("muted") ? "muted" : "";
 		if (this.isShown && this.options.backdrop)
 		{
 			var doAnimate = $.support.transition && animate;
 
-			this.$backdrop = $('<div class="bg-black ' + animate + '" />')
-				.appendTo(document.body);
+			this.$backdrop = $("<div class='bg-black " + animate + "' />").appendTo(document.body);
 
 			this.$element.on('click.dismiss.lightbox', $.proxy(function (e)
 			{
